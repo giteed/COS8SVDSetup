30ecd88ca351d4136b9838f7820bdbb78601f0b4
giteed <33794654+giteed@users.noreply.github.com>
2023-03-09 20:14:27 +0000

    .

--- bin/cos8svdsetup/core/kernel-up.sh
+++ bin/cos8svdsetup/core/kernel-up.sh
@@ -8,26 +8,26 @@
 # Скрипт для чистого VDS сервера на базе centos 8 stream, с ядром vmlinuz-4.18.0-448.el8.x86_64 , который установит на него новое ядро vmlinuz-6.2.2-1.el8.elrepo.x86_64
 
 # Смотрим какое ядро используется в данный момент:
-	echo -en " Смотрим какое ядро используется в данный момент: "
+	echo -en "\n Смотрим какое ядро используется в данный момент: "
 	uname -r ; echo ;
 
 # Первым шагом обновим все установленные пакеты:
-	sudo yum update -y ;
+	sudo yum update -y ; echo ;
 
 # Установим необходимые зависимости для установки нового ядра:
-	sudo yum install -y elfutils-libelf-devel gcc gcc-c++ make rpm-build ;
+	sudo yum install -y elfutils-libelf-devel gcc gcc-c++ make rpm-build ;  echo ;
 
 # Скачаем и установим репозиторий ELRepo:
-	sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org ;
-	sudo rpm -Uvh https://www.elrepo.org/elrepo-release-8.0-2.el8.elrepo.noarch.rpm ;
+	sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org ; echo ;
+	sudo rpm -Uvh https://www.elrepo.org/elrepo-release-8.0-2.el8.elrepo.noarch.rpm ; echo ;
 	# sudo rpm -Uvh https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
 	# dnf install https://www.elrepo.org/elrepo-release-8.0-2.el8.elrepo.noarch.rpm
 
 # Установим новое ядро из репозитория ELRepo:
-	sudo yum --enablerepo=elrepo-kernel install -y kernel-ml ;
+	sudo yum --enablerepo=elrepo-kernel install -y kernel-ml ; echo ;
 
 # Обновим конфигурацию загрузчика:
-	sudo grub2-mkconfig -o /boot/grub2/grub.cfg ;
+	sudo grub2-mkconfig -o /boot/grub2/grub.cfg ; echo ;
 
 # Перезагрузим сервер:
 echo ;
@@ -42,6 +42,6 @@ echo -en " Подождите, сервер перезагружается...
 	sudo reboot
 
 # После перезагрузки, можно убедиться, что новое ядро было успешно установлено, выполнив команду:
-uname -r ;
+uname -r ; echo ;
 
 # Команда должна вернуть 6.2.2-1.el8.elrepo.x86_64, что означает, что новое ядро успешно установлено и загружено.
