9c62ca75827e562d383f66e20b6dd801d54b442f
giteed <33794654+giteed@users.noreply.github.com>
2023-03-11 00:12:28 +0000

    .

--- bin/cos8svdsetup/functions/.load_function.sh
+++ bin/cos8svdsetup/functions/.load_function.sh
@@ -2,5 +2,6 @@
 #!/bin/bash
 
 
- # --> Прочитать настройки из:
+ # --> Прочитать настройки из файла с функциями:
  . /root/vdsetup.2/bin/functions/bat_or_bat_not_installed.sh
+ . /root/vdsetup.2/bin/functions/press_enter.sh
