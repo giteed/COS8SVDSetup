721e7ee088062bdb38c961ee05eccbe3c03c0d2b
giteed <33794654+giteed@users.noreply.github.com>
2023-03-11 05:35:38 +0000

    .

--- bin/cos8svdsetup/utility/epel_repo_pack.sh
+++ bin/cos8svdsetup/utility/epel_repo_pack.sh
@@ -10,9 +10,9 @@
   {
  
 	
-	ttb=$( echo -e "\n Установка дополнительных пакетов: epel-release, iptables, python3, ruby, npm, unzip, hstr, lsof, screen, tar, p7zip, mc, nano, whois, wget, curl, atop, htop, nethogs, bpytop, iftop, stacer, yum-utils, net-tools, network-scripts, git, dialog, mlocate qrencode, ncdu ranger tldr. \n" ) && bpn_p_lang ; echo ; echo ;
+	ttb=$( echo -e "\n Установка дополнительных пакетов: epel-release, iptables, python3, ruby, npm, unzip, hstr, lsof, screen, tar, p7zip, mc, nano, whois, wget, curl, atop, htop, nethogs, bpytop, iftop, stacer, yum-utils, net-tools, network-scripts, git, dialog, mlocate qrencode, ncdu, ranger, tldr, whois. \n" ) && bpn_p_lang ; echo ; echo ;
 	
-	dnf install git tar curl wget -y 
+	dnf install git tar curl wget whois -y 
 	dnf install -y epel-release 
 	dnf install -y net-tools network-scripts 
 	dnf install -y dialog mlocate ncdu ranger tldr 
