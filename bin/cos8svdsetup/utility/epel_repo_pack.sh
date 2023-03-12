123b6fcfa732c04466215acedb3a5354cb523ae0
giteed <33794654+giteed@users.noreply.github.com>
2023-03-12 22:54:09 +0000

    .

--- bin/cos8svdsetup/utility/epel_repo_pack.sh
+++ bin/cos8svdsetup/utility/epel_repo_pack.sh
@@ -10,19 +10,19 @@
 	
 	press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
 	
-	dnf install -y epel-release yum-utils npm || ( error_MSG ; ) ;
-	dnf install -y net-tools network-scripts iptables || ( error_MSG ; ) ;
-	dnf install -y dialog mlocate ncdu ranger tldr || ( error_MSG ; ) ;
-	dnf install -y youtube-dl || ( error_MSG ; ) ;
-	dnf install -y git tar curl wget || ( error_MSG ; ) 
-	dnf install -y whois || ( error_MSG ; ) ;
-	dnf install -y atop htop bpytop iftop stacer lsof nethogs  || ( error_MSG ; ) ;
-	dnf install -y python3 ruby  || ( error_MSG ; ) ;
-	dnf install -y mc nano hstr ncdu || ( error_MSG ; ) ;
-	dnf install -y unzip p7zip || ( error_MSG ; ) ;
-	dnf install -y screen qrencode || ( error_MSG ; ) ;
-	dnf install -y @perl perl perl-Net-SSLeay perl-Encode-Detect openssl || ( error_MSG ; ) ; 
-	dnf install -y perl-IO-Tty || ( error_MSG ; ) ;
+	dnf install -y epel-release yum-utils npm || ( error_MSG ; ) ; echo ;
+	dnf install -y net-tools network-scripts iptables || ( error_MSG ; ) ; echo ;
+	dnf install -y dialog mlocate ncdu ranger tldr || ( error_MSG ; ) ; echo ;
+	dnf install -y youtube-dl || ( error_MSG ; ) ; echo ;
+	dnf install -y git tar curl wget || ( error_MSG ; ) ; echo ;
+	dnf install -y whois || ( error_MSG ; ) ; echo ;
+	dnf install -y atop htop bpytop iftop stacer lsof nethogs  || ( error_MSG ; ) ; echo ;
+	dnf install -y python3 ruby  || ( error_MSG ; ) ; echo ;
+	dnf install -y mc nano hstr ncdu || ( error_MSG ; ) ; echo ;
+	dnf install -y unzip p7zip || ( error_MSG ; ) ; echo ;
+	dnf install -y screen qrencode || ( error_MSG ; ) ; echo ;
+	dnf install -y @perl perl perl-Net-SSLeay perl-Encode-Detect openssl || ( error_MSG ; ) ; echo ;
+	dnf install -y perl-IO-Tty || ( error_MSG ; ) ; echo ;
 	
 	echo -e "\n Установка дополнительных пакетов завершена!" 
 	echo -e " ($( green_tick )) - packages plus${RED}   | ${NC}посмотреть список пакетов в системе ypr -rl"   
