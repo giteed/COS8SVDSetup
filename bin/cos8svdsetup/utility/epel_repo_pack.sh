e05cf5313568355d7b30a04eaab909e4afb6ddf7
giteed <33794654+giteed@users.noreply.github.com>
2023-03-12 22:59:54 +0000

    .

--- bin/cos8svdsetup/utility/epel_repo_pack.sh
+++ bin/cos8svdsetup/utility/epel_repo_pack.sh
@@ -13,14 +13,14 @@
 	wget, curl, atop, htop, nethogs, bpytop, iftop, 
 	stacer, yum-utils, net-tools, network-scripts, git, 
 	dialog, mlocate qrencode, ncdu, ranger, tldr, whois, 
-	youtube-dl. \n" ) && bpn_p_lang ; echo ;
+	youtube-dl, ffmpeg. \n" ) && bpn_p_lang ; echo ;
 	
 	press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
 	
 	dnf install -y epel-release yum-utils npm || ( error_MSG ; ) ; echo ;
 	dnf install -y net-tools network-scripts iptables || ( error_MSG ; ) ; echo ;
 	dnf install -y dialog mlocate ncdu ranger tldr || ( error_MSG ; ) ; echo ;
-	dnf install -y youtube-dl || ( error_MSG ; ) ; echo ;
+	dnf install -y youtube-dl ffmpeg || ( error_MSG ; ) ; echo ;
 	dnf install -y git tar curl wget || ( error_MSG ; ) ; echo ;
 	dnf install -y whois || ( error_MSG ; ) ; echo ;
 	dnf install -y atop htop bpytop iftop stacer lsof nethogs  || ( error_MSG ; ) ; echo ;
@@ -38,3 +38,34 @@
   epel_repo_pack ;
 
 exit 0 ; 
+
+
+Скачивание YouTube видео youtube-dl
+
+После установки, достаточно просто указать ссылку, youtube-dl загрузит видео и сохранит его в формате mp4 / flv.
+
+
+youtube-dl https://www.youtube.com/watch?v=dQw4w9WgXcQ
+Скачивание видео с YouTube в разных форматах
+
+Большинство видео на YouTube доступны в разных форматах и разном качестве. Для начала необходимо проверить какие форматы доступны
+
+
+youtube-dl -F https://www.youtube.com/watch?v=dQw4w9WgXcQ
+youtube-dl -F https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+Выбираем необходимый формат и скачиваем его
+
+
+youtube-dl -f 140 https://www.youtube.com/watch?v=dQw4w9WgXcQ
+Пакетное скачивание видео с YouTube
+
+для скачивания множества видео с YouTube используем параметр -a
+
+youtube-dl -a video.list.txt
+Извлечь аудио (MP3) из видео YouTube
+
+Если необходимо извлечь звук видео и сохранить его в аудио формате (mp3), необходимо установить ffmpeg, после этого можно будет конвертировать видео YouTube в аудио файлы.
+
+
+youtube-dl https://www.youtube.com/watch?v=dQw4w9WgXcQ --extract-audio --audio-format mp3
