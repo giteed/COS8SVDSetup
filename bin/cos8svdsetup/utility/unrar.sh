d5bbb92dc2dd1fb241a600f3db0e24e87be9d98f
giteed <33794654+giteed@users.noreply.github.com>
2023-03-12 18:12:17 +0000

    .

--- bin/cos8svdsetup/utility/unrar.sh
+++ bin/cos8svdsetup/utility/unrar.sh
@@ -15,9 +15,9 @@
 function UnrarIN() 
 { 
 	( echo -e " Установка forensics REPO, Unrar. \n" ;
-	mkdir -p /tmp/installer/rar && cd /tmp/installer/rar && wget https://forensics.cert.org/cert-forensics-tools-release-el8.rpm && rpm -Uvh cert-forensics-tools-release*rpm && yum-config-manager --enable forensics ) &>/dev/null ;
-	( ( dnf install unrar -y ) &>/dev/null || ( ( echo -en " функция установки UnrarIN: " )  && ( error_MSG ; ) ) ) ;
-	( cd /tmp/ && rm -rf /tmp/installer/rar ) &>/dev/null ;
+	mkdir -p /tmp/installer/rar && cd /tmp/installer/rar && wget https://forensics.cert.org/cert-forensics-tools-release-el8.rpm && rpm -Uvh cert-forensics-tools-release*rpm && yum-config-manager --enable forensics )  ;
+	( ( dnf install unrar -y )  || ( ( echo -en " функция установки UnrarIN: " )  && ( error_MSG ; ) ) ) ;
+	( cd /tmp/ && rm -rf /tmp/installer/rar )  ;
 	
 	( echo -en " " &&  (( unrar -v | grep UNRAR | awk ' { print $1, $2, $3 }' ) 2>/dev/null ) && ( echo -e "\n $( green_tick )${GREEN} Forensics REPO, Unrar установлены.${NC}\n") && ( echo -e " ($( green_tick )) - forensics-Unrar${RED} | $( green_OK ) ypr -rl " )) || ( ( echo -en " функция установки UnrarIN: " )  && ( error_MSG ; ) ) ;
 }
