6cf515c49ef774dabb99ea399c16f5836e891ff5
giteed <33794654+giteed@users.noreply.github.com>
2023-03-14 01:34:01 +0000

    .

--- version.sh
+++ version.sh
@@ -2,10 +2,8 @@
 
 # Source global definitions
 # --> Прочитать настройки из /root/.bashrc
-. /root/.bashrc
+#. /root/.bashrc
 
-# --> Этот функция проверяет, запущен ли скрипт с правами суперпользователя (root) в Linux.
-. /root/vdsetup.2/bin/functions/run_as_root.sh
 
 
 # Получаем номер последнего коммита
