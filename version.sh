227cca47247206b610940ff66796e404a31a10f6
giteed <33794654+giteed@users.noreply.github.com>
2023-03-14 00:21:33 +0000

    .

---
+++ version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Source global definitions
+# --> Прочитать настройки из /root/.bashrc
+. /root/.bashrc
+
+# --> Этот функция проверяет, запущен ли скрипт с правами суперпользователя (root) в Linux.
+. /root/vdsetup.2/bin/functions/run_as_root.sh
+
+
+# Получаем номер последнего коммита
+commit=$(git rev-parse --short HEAD)
+
+# Получаем дату создания последнего коммита в формате ГГГГММДД
+date=$(git log -1 --format=%cd --date=format:%Y%m%d)
+
+# Определяем версию
+version="$date-$commit"
+
+# Выводим версию
+echo "Версия скрипта: $version"
+
+
+exit 0
