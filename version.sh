39479b269bed0a4273f891e5263b15259b09ed5c
giteed <33794654+giteed@users.noreply.github.com>
2023-03-14 03:52:38 +0000

    version to README.md

--- version.sh
+++ version.sh
@@ -8,6 +8,7 @@ date=$(git log -1 --format=%cd --date=format:%Y%m%d)
 
 # Определяем версию
 version="$date-$commit"
+sed -i '' "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $VERSION/" README.md
 
 # Выводим версию
 echo "Версия скрипта: $version"
