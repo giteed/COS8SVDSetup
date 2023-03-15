eb1551ad37cb02788203b829e53b76d841c38f40
giteed <33794654+giteed@users.noreply.github.com>
2023-03-15 16:37:13 +0000

    .

--- version.sh
+++ version.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # --> Получаем номер последнего коммита
-	LAST_COMMIT=$(git rev-parse --short HEAD)
+LAST_COMMIT=$(git rev-parse --short HEAD)
 
 # --> Получаем текущую дату
-	DATE=$(date '+%Y-%m-%d %H:%M:%S')
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 # --> Получаем текущую версию
-	VERSION=$(grep 'VERSION=' version.sh | sed 's/VERSION=//')
+VERSION=$(grep 'VERSION=' version.sh | sed 's/VERSION=//')
 
 # --> Увеличиваем номер версии
-	VERSION=$(echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' OFS=. )
+VERSION=$(echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' OFS=. )
 
 # --> Обновляем номер версии в скрипте
-	sed -i '' "s/VERSION=.*/VERSION=$VERSION/" version.sh
+sed -i "s/VERSION=.*/VERSION=$VERSION/" version.sh
 
 # --> Обновляем номер версии в README.md
-	sed -i '' "s/Version: [0-9]\+\.[0-9]\+\.[0-9]\+/Version: $VERSION/" README.md
+sed -i "s/Version: [0-9]\+\.[0-9]\+\.[0-9]\+/Version: $VERSION/" README.md
 
 # --> Обновляем номер коммита и дату в version.sh
-	sed -i '' "1i\
+sed -i "1i\\
 # Version $VERSION, last commit: $LAST_COMMIT, $DATE" version.sh
 
 # --> Обновляем номер коммита и дату в README.md
-	sed -i '' "s/last commit: [0-9a-f]\+/last commit: $LAST_COMMIT, $DATE/" README.md
+sed -i "s/last commit: [0-9a-f]\+/last commit: $LAST_COMMIT, $DATE/" README.md
 
-	exit 0
+exit 0
+
 
 
