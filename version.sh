68d24a0753ae42335b3e7220e3837e33a93dbc70
giteed <33794654+giteed@users.noreply.github.com>
2023-03-15 21:33:40 +0000

    .

--- version.sh
+++ version.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Получаем текущую версию
+VERSION=$(grep 'Version' VERSION | awk '{print $2}')
+
+# Увеличиваем номер версии
+VERSION=$(echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' OFS=. )
+
+# Обновляем номер версии в файле VERSION
+sed -i '' "s/Version.*/Version $VERSION/" VERSION
+
+# Обновляем номер версии в файле README.md
+sed -i '' "s/Version.*/Version $VERSION/" README.md
+
+# Добавляем изменения в Git
+git add VERSION README.md
+
+# Делаем коммит с сообщением о версии
+git commit -m "Bump version to $VERSION"
+
+# Завершаем работу скрипта
+exit 0
+
+
+
+#!/bin/bash
+
 # --> Получаем номер последнего коммита
 LAST_COMMIT=$(git rev-parse --short HEAD)
 
