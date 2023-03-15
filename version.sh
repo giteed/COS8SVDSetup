22cf6ceb2d7937590da4aa85d2e8f689106499d0
giteed <33794654+giteed@users.noreply.github.com>
2023-03-15 16:06:11 +0000

    .

--- version.sh
+++ version.sh
@@ -7,20 +7,20 @@
 	DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 # --> Получаем текущую версию
-	VERSION=$(grep 'VERSION=' script.sh | sed 's/VERSION=//')
+	VERSION=$(grep 'VERSION=' version.sh | sed 's/VERSION=//')
 
 # --> Увеличиваем номер версии
 	VERSION=$(echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' OFS=. )
 
 # --> Обновляем номер версии в скрипте
-	sed -i '' "s/VERSION=.*/VERSION=$VERSION/" script.sh
+	sed -i '' "s/VERSION=.*/VERSION=$VERSION/" version.sh
 
 # --> Обновляем номер версии в README.md
 	sed -i '' "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $VERSION/" README.md
 
-# --> Обновляем номер коммита и дату в script.sh
+# --> Обновляем номер коммита и дату в version.sh
 	sed -i '' "1i\
-# Version $VERSION, last commit: $LAST_COMMIT, $DATE" script.sh
+# Version $VERSION, last commit: $LAST_COMMIT, $DATE" version.sh
 
 # --> Обновляем номер коммита и дату в README.md
 	sed -i '' "s/last commit: [0-9a-f]\+/last commit: $LAST_COMMIT, $DATE/" README.md
