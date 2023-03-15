961c7f82fab05487439594fe266a83927f421ad3
giteed <33794654+giteed@users.noreply.github.com>
2023-03-15 21:22:42 +0000

    .

--- version.sh
+++ version.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# --> Получаем номер последнего коммита
+LAST_COMMIT=$(git rev-parse --short HEAD)
+
+# --> Получаем текущую дату
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+# --> Получаем текущую версию
+VERSION=$(grep 'VERSION=' VERSION | sed 's/VERSION=//')
+
+# --> Увеличиваем номер версии
+VERSION=$(echo $VERSION | awk -F. '{$NF = $NF + 1;} 1' OFS=.3)
+
+# --> Обновляем номер версии в скрипте
+sed -i '' "s/VERSION=.*/VERSION=$VERSION/" VERSION
+
+# --> Обновляем номер версии в README.md
+sed -i '' "s/Version [0-9]\+\.[0-9]\+\.[0-9]\+ Last commit:/Version $VERSION Last commit:/" README.md
+
+# --> Обновляем номер коммита и дату в version.sh
+sed -i '' "1i\\
+# Version $VERSION, last commit: $LAST_COMMIT, $DATE" VERSION
+
+# --> Обновляем номер коммита и дату в README.md
+sed -i '' "s/last commit: [0-9a-f]\+/last commit: $LAST_COMMIT, $DATE/" README.md
+
+exit 0
+
+
+#!/bin/bash
+
 # Получаем номер последнего коммита
 LAST_COMMIT=$(git rev-parse --short HEAD)
 
