4821ffb61c2bf3c676a803268c47cd178635ffe7
giteed <33794654+giteed@users.noreply.github.com>
2023-03-17 22:15:26 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COS8SVDSetup 
-Version 0.1.64
+Version 0.1.65
 
 ![vdsetup_logo.png](/images/vdsetup_logo.png)
 ## Script for initial preparation of fresh VDS based on CentOS 8 Stream
--- bin/cos8svdsetup/preloader.sh
+++ bin/cos8svdsetup/preloader.sh
@@ -4,6 +4,52 @@
 # --> Прочитать настройки из /root/.bashrc
 #. /root/.bashrc
 
+
+# --> функции работы с текстовым процессором bat для подсветки синтаксиса терминала.
+# --> если bat не установлен, функция покажет любой текст отправленый в переменную ttb в обычном виде.
+   if  [[ $lang == "" ]] ; then lang="nix" ; fi ;
+
+   #------------------------------------
+   # bat installed/not_installed
+   #------------------------------------  
+   
+   function bpn_p_lang() {
+	  
+	 ( echo -e "${ttb}" | bat --paging=never -l ${lang} -p 2>/dev/null || echo -e "$ttb" ) 
+	  ttb="" ;
+	}
+
+   function bpal_p_lang() {
+	 
+	 ( echo -e "${ttb}" | bat --paging=always -l ${lang} -p 2>/dev/null || echo -e "$ttb" ) 
+	 ttb="" ;
+	}
+   
+   function lang_x() {
+	  lang=$1 ;
+	  if [[ $1 == "" ]] ; then lang=cr ; fi ;
+   }
+   
+   function lang_nix() {
+	  lang=nix
+   }
+   function lang_cr() {
+	  lang=cr
+   }   
+   
+   function lang_bash() {
+	  lang=bash ;
+   }
+   
+   function lang_help() {
+	  lang=help ;
+   }
+   
+   
+   
+
+
+
 lang_x 2>/dev/null ;
 
 # Определение функции run_as_root()
