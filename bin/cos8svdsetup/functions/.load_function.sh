d4f13af84ff30d33ab5a22576de618891b88ed17
giteed <33794654+giteed@users.noreply.github.com>
2023-03-18 00:16:55 +0000

    .

--- bin/cos8svdsetup/functions/.load_function.sh
+++ bin/cos8svdsetup/functions/.load_function.sh
@@ -2,20 +2,20 @@
 
 # --> Прочитать настройки из файла с функциями:
 # --> Работа с текстовым процессором bat для подсветки синтаксиса терминала.
-	sudo /root/vdsetup.2/bin/functions/bat_or_bat_not_installed.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/bat_or_bat_not_installed.sh'
 # --> Этот код, запрашивает пользователя подтверждение для продолжения.
-	sudo /root/vdsetup.2/bin/functions/press_enter.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/press_enter.sh'
 # --> Поиск программы/файла локально и в репо по 7 командам.
-	sudo /root/vdsetup.2/bin/functions/ypr.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/ypr.sh'
 # --> Поиск информации о программе сразу по 11 командам.
-	sudo /root/vdsetup.2/bin/functions/ww.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/ww.sh'
 # --> Информация о ip адресах и некоторые другие информационные функции о системе.
-	sudo /root/vdsetup.2/bin/functions/ip_other_info_func.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/ip_other_info_func.sh'
 # --> клонирование из gh repo запуск /preloader.sh (в планах частичная синхронизация)
-	sudo /root/vdsetup.2/bin/functions/vsync.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/vsync.sh'
 # --> Проверяет или устанавливает проверяемые компоненты.	
-	sudo /root/vdsetup.2/bin/functions/check_or_install.sh
+	sudo -E bash -c '. /root/vdsetup.2/bin/functions/check_or_install.sh'
 
 
 # --> Этот функция проверяет, запущен ли скрипт с правами суперпользователя (root) в Linux.
-#	sudo /root/vdsetup.2/bin/functions/run_as_root.sh
+#	sudo -E bash -c '. /root/vdsetup.2/bin/functions/run_as_root.sh'
--- bin/cos8svdsetup/styles/.load_styles.sh
+++ bin/cos8svdsetup/styles/.load_styles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # --> Прочитать настройки стилей из:
-sudo /root/vdsetup.2/bin/styles/colored_unicode_characters.sh
-sudo /root/vdsetup.2/bin/styles/terminal_colors.sh
+sudo -E bash -c '. /root/vdsetup.2/bin/styles/colored_unicode_characters.sh'
+sudo -E bash -c '. /root/vdsetup.2/bin/styles/terminal_colors.sh'
