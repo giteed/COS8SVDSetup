e0db732aed8f8388313e9a99de0963e4cd259541
giteed <33794654+giteed@users.noreply.github.com>
2023-03-18 01:36:46 +0000

    .

--- bin/cos8svdsetup/functions/vsync.sh
+++ bin/cos8svdsetup/functions/vsync.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
 # --> Этот функция проверяет, запущен ли скрипт с правами суперпользователя (root) в Linux.
-#. /root/vdsetup.2/bin/functions/run_as_root.sh
+. /root/vdsetup.2/bin/functions/run_as_root.sh
 
-
- function rm_clone() {
-    cd /root/ ; rm -rf /root/COS8SVDSetup ; (git clone https://github.com/giteed/COS8SVDSetup.git /root/COS8SVDSetup) ; (/root/COS8SVDSetup/bin/cos8svdsetup/preloader.sh) ; return ;
-    
+# --> Функция для удаления старой версии репозитория и клонирования свежей версии из GitHub После клонирования запускается скрипт /preloader.sh
+function rm_clone() {
+# --> переходим в корневую директорию
+        cd /root/ ;
+# --> удаляем старую версию репозитория, если есть
+        rm -rf /root/COS8SVDSetup ; 
+# --> клонируем репозиторий из GitHub
+        (git clone https://github.com/giteed/COS8SVDSetup.git /root/COS8SVDSetup) ;
+# --> запускаем скрипт /preloader.sh
+        (/root/COS8SVDSetup/bin/cos8svdsetup/preloader.sh) ; 
+        return ; # --> завершаем функцию
 }
 
+# --> Функция для синхронизации репозитория и обновления локальной версии.
+# --> Если локальная версия отсутствует, то вызывается функция rm_clone для ее создания
+# --> После синхронизации копируются файлы из репозитория в /root/vdsetup.2/bin/
+# --> и переносится .bashrc, .bash_profile, и .bash_aliases в домашнюю директорию /root/
 
-# --> клонирование из gh repo запуск /preloader.sh (в планах частичная синхронизация)
- function vsync()
- {
+function vsync() {
+# --> Внутренняя функция для синхронизации репозитория и обновления локальной версии   
     function _vsync() {
-        
-        
+# --> переходим в директорию репозитория
         cd /root/COS8SVDSetup ;
-        # сохранение изменений в стэш
+# --> сохраняем изменения в stash
         git status ;
         git stash ;
+# --> синхронизируем репозиторий с GitHub
         gh repo sync --branch=main ;
-        sudo cp -a /root/COS8SVDSetup/bin/cos8svdsetup/. /root/vdsetup.2/bin #2>/dev/null
-        # --> скопировать файлы конфигурации в корневой каталог
-        
+# --> Копирование файлов из репозитория в /root/vdsetup.2/bin
+        sudo cp -a /root/COS8SVDSetup/bin/cos8svdsetup/. /root/vdsetup.2/bin ;
+# --> копируем .bashrc, .bash_profile и .bash_aliases в домашнюю директорию /root/
         sudo cp -a /root/COS8SVDSetup/.bashrc /root/ ;
         sudo cp -a /root/COS8SVDSetup/.bash_profile /root/ ;
         sudo cp -a /root/COS8SVDSetup/.bash_aliases /root/ ;
         cd ~
+# --> Наконец, запускается .bashrc для применения изменений
         source /root/.bashrc
     }
-    
-
-    
+# --> Включение SSH протокола для работы с репозиторием
     gh config set -h github.com git_protocol ssh ;
-    
+# --> Проверка наличия директории /root/COS8SVDSetup, если директории нет - клонируем репозиторий rm_clone, иначе запускаем _vsync
     [[ -n $(ls /root/COS8SVDSetup) ]] 2>/dev/null && _vsync || rm_clone ;
+}
 
- }
- 
