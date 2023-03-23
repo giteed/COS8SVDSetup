ed365a5a35efc9d994c26e2f725342f4db14f0da
giteed <33794654+giteed@users.noreply.github.com>
2023-03-23 21:21:05 +0000

    .

--- bin/cos8svdsetup/functions/find.sh
+++ bin/cos8svdsetup/functions/find.sh
@@ -41,3 +41,23 @@
    function gkill() {
       /root/vdsetup.2/bin/utility/k-i-l-l_b-y_k-e-y-w-o-r-d.sh ;
    }
+   
+   
+   # Если ali используется без аргументов == "" то просто alias, если ali с аргументами != "", то подставляются они после alias "$Val2"
+   function ali() # alias | bat
+   {
+   
+      if [ "$*" == "" ] ; 
+         then Val1="" ;
+         echo -e "$green""\n***"$NC" Все Алиасы $(whoami)"$green" ***"$NC"\n " ;
+         ( alias | bat -p --paging=never -l .bash_aliases ) || ( alias ) ;
+         return ;
+      fi
+         if [ "$*" != "" ] ; 
+         then Val2="$*"
+         echo -e "$green""\n***"$NC" Алиас "$cyan""$Val2""$NC" для $(whoami)"$green" ***"$NC"\n " ;
+         alias "$Val2" | bat -p --paging=never -l .bash_aliases ||  ( alias "$Val2" ) ;
+         return ;
+      fi	
+      
+   }
