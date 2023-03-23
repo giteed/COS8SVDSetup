7e9274c13363672d8c4ee387be5e939625864ca0
giteed <33794654+giteed@users.noreply.github.com>
2023-03-23 16:04:26 +0000

    .

--- bin/cos8svdsetup/utility/del.sh
+++ bin/cos8svdsetup/utility/del.sh
@@ -7,6 +7,8 @@
 # --> Этот ссылка на функцию проверяет, запущен-ли скрипт с правами суперпользователя (root) в Linux.
 #. /root/vdsetup.2/bin/functions/run_as_root.sh
 
+
+# --> Полное удаление VDSetup (не удаляет того, что было установлено скриптом)
 sudo rm -rf /root/COS8SVDSetup
 sudo rm -rf /root/vdsetup.2
 sudo rm /root/.bashrc
