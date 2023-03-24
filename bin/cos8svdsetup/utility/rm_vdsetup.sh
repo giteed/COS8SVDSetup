5d8e782bb0e84f108b240676ab6832b5e25f4fc2
giteed <33794654+giteed@users.noreply.github.com>
2023-03-24 07:25:43 +0000

    .

--- bin/cos8svdsetup/utility/rm_vdsetup.sh
+++ bin/cos8svdsetup/utility/rm_vdsetup.sh
@@ -14,20 +14,6 @@ function remove_vdsetup() {
 	sudo rm /root/.bashrc
 	sudo rm /root/.bash_aliases
 	sudo rm /root/.bash_profile
-	
-	 
-	ttb=$(echo -e " 
-	 ⎧  
-	 | # sudo rm -rf /root/COS8SVDSetup
-	 | # sudo rm -rf /root/vdsetup.2
-	 | # sudo rm /root/.bashrc
-	 | # sudo rm /root/.bash_aliases
-	 | # udo rm /root/.bash_profile
-	 |
-	 | VDSetup removed! To reinstall, enter: # vsync
-	 | или:
-	 | # cd /root/ ; (dnf -y install rsync rsync-daemon git mc); (echo); (git clone https://github.com/giteed/COS8SVDSetup.git /root/COS8SVDSetup) ; (/root/COS8SVDSetup/bin/cos8svdsetup/preloader.sh) ;
-	 ⎩" ) && lang_cr && bpn_p_lang ; 
 }
 
 
@@ -38,8 +24,22 @@ ttb=$(echo -e "
  ⎩" ) && lang_cr && bpn_p_lang ; 
  
  press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
-
+ 
  remove_vdsetup ;
+  
+ttb=$(echo -e " 
+ ⎧  
+ | # sudo rm -rf /root/COS8SVDSetup
+ | # sudo rm -rf /root/vdsetup.2
+ | # sudo rm /root/.bashrc
+ | # sudo rm /root/.bash_aliases
+ | # udo rm /root/.bash_profile
+ |
+ | VDSetup removed! To reinstall, enter: # vsync
+ | или:
+ | # cd /root/ ; (dnf -y install rsync rsync-daemon git mc); (echo); (git clone https://github.com/giteed/COS8SVDSetup.git /root/COS8SVDSetup) ; (/root/COS8SVDSetup/bin/cos8svdsetup/preloader.sh) ;
+ ⎩" ) && lang_cr && bpn_p_lang ; 
+
 
 
 exit 0 ; 
