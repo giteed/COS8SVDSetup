34a609694d749932d132342e883ca597fbdcb5dd
giteed <33794654+giteed@users.noreply.github.com>
2023-03-24 07:23:23 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COS8SVDSetup 
-Version 0.1.145
+Version 0.1.146
 
 ![vdsetup_logo.png](/images/vdsetup_logo.png)
 ## Script for initial preparation of fresh VDS based on CentOS 8 Stream
--- bin/cos8svdsetup/utility/rm_vdsetup.sh
+++ bin/cos8svdsetup/utility/rm_vdsetup.sh
@@ -7,13 +7,29 @@
 # --> Этот ссылка на функцию проверяет, запущен-ли скрипт с правами суперпользователя (root) в Linux.
 . /root/vdsetup.2/bin/functions/run_as_root.sh
 
+function remove_vdsetup() {
+	# --> Полное удаление VDSetup (не удаляет того, что было установлено скриптом)
+	sudo rm -rf /root/COS8SVDSetup
+	sudo rm -rf /root/vdsetup.2
+	sudo rm /root/.bashrc
+	sudo rm /root/.bash_aliases
+	sudo rm /root/.bash_profile
+	
+	 
+	ttb=$(echo -e " 
+	 ⎧  
+	 | # sudo rm -rf /root/COS8SVDSetup
+	 | # sudo rm -rf /root/vdsetup.2
+	 | # sudo rm /root/.bashrc
+	 | # sudo rm /root/.bash_aliases
+	 | # udo rm /root/.bash_profile
+	 |
+	 | VDSetup removed! To reinstall, enter: # vsync
+	 | или:
+	 | # cd /root/ ; (dnf -y install rsync rsync-daemon git mc); (echo); (git clone https://github.com/giteed/COS8SVDSetup.git /root/COS8SVDSetup) ; (/root/COS8SVDSetup/bin/cos8svdsetup/preloader.sh) ;
+	 ⎩" ) && lang_cr && bpn_p_lang ; 
+}
 
-# --> Полное удаление VDSetup (не удаляет того, что было установлено скриптом)
-sudo rm -rf /root/COS8SVDSetup
-sudo rm -rf /root/vdsetup.2
-sudo rm /root/.bashrc
-sudo rm /root/.bash_aliases
-sudo rm /root/.bash_profile
 
 ttb=$(echo -e " 
  ⎧ 
@@ -22,20 +38,8 @@ ttb=$(echo -e "
  ⎩" ) && lang_cr && bpn_p_lang ; 
  
  press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
- 
-ttb=$(echo -e " 
- ⎧  
- | # sudo rm -rf /root/COS8SVDSetup
- | # sudo rm -rf /root/vdsetup.2
- | # sudo rm /root/.bashrc
- | # sudo rm /root/.bash_aliases
- | # udo rm /root/.bash_profile
- |
- | VDSetup removed! To reinstall, enter: # vsync
- | или:
- | # cd /root/ ; (dnf -y install rsync rsync-daemon git mc); (echo); (git clone https://github.com/giteed/COS8SVDSetup.git /root/COS8SVDSetup) ; (/root/COS8SVDSetup/bin/cos8svdsetup/preloader.sh) ;
- ⎩" ) && lang_cr && bpn_p_lang ; 
 
+ remove_vdsetup ;
 
 
 exit 0 ; 
