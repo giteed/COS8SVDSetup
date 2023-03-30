fbe2a1c0a74fbaf6b9b89bbf179a750f2f1b492d
giteed <33794654+giteed@users.noreply.github.com>
2023-03-30 18:54:28 +0000

    .

--- bin/cos8svdsetup/nginx_install/nginx-install.sh
+++ bin/cos8svdsetup/nginx_install/nginx-install.sh
@@ -220,11 +220,11 @@
 			{
 				
 				echo -e -n "\n	
-		${RED}|${nc} Заменить текущий конфиг файл /etc/nginx/nginx.conf "${GREEN}"на преднастроенный"${NC}"?\n
-		${RED}|${nc} Если нет, "${RED}"Enter"${NC}" 
-		${RED}|${nc} Eсли да, введите: "${GREEN}"yes"$NC"
+	${RED}|${nc} Заменить текущий конфиг файл /etc/nginx/nginx.conf "${GREEN}"на преднастроенный"${NC}"?\n
+	${RED}|${nc} Если нет, "${RED}"Enter"${NC}" 
+	${RED}|${nc} Eсли да, введите: "${GREEN}"yes"$NC"
 				
-		["$RED"$(im)"$NC"@"$GRAY""$(hostname)""$NC"] "$NC"<<< "$RED"# "$NC""$GREEN""
+	["$RED"$(im)"$NC"@"$GRAY""$(hostname)""$NC"] "$NC"<<< "$RED"# "$NC""$GREEN""
 				read nginxconfyes
 				
 			if [[ "$nginxconfyes" == "yes" ]]
