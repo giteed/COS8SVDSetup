74799477e876862c6599a0bfe7622df101f16e84
giteed <33794654+giteed@users.noreply.github.com>
2023-03-31 16:52:32 +0000

    .

---
+++ bin/cos8svdsetup/utility/auth_screen_warn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Source global definitions
+# --> Прочитать настройки из /root/.bashrc
+#. /root/.bashrc
+
+# --> Этот ссылка на функцию проверяет, запущен-ли скрипт с правами суперпользователя (root) в Linux.
+#. /root/vdsetup.2/bin/functions/run_as_root.sh
+
+
+
+while true; do echo "test"; sleep 10; done
+
+
+
+
+
+
+
+exit 0
