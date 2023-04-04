7f34a9d37995a27cdb0b1149a525bd1340f04a67
giteed <33794654+giteed@users.noreply.github.com>
2023-04-04 23:27:52 +0000

    .

--- bin/cos8svdsetup/utility/auth/ssh/Auth_Warn_SSH_start.sh
+++ bin/cos8svdsetup/utility/auth/ssh/Auth_Warn_SSH_start.sh
@@ -9,17 +9,18 @@
 
 
 
+
 function check_unit_Auth_Warn_SSH_service() {
 
-if test -S /etc/systemd/system/Auth_Warn_SSH.service; then
-  echo " Unit file /etc/systemd/system/Auth_Warn_SSH.service exists."
-else
-  echo " Create, enable and start Unit /etc/systemd/system/Auth_Warn_SSH.service"
-   cp /root/vdsetup.2/bin/units_systemd/Auth_Warn_SSH.service /etc/systemd/system/Auth_Warn_SSH.service
-   systemctl daemon-reload
-   systemctl enable Auth_Warn_SSH.service
-   systemctl start Auth_Warn_SSH.service || systemctl restart Auth_Warn_SSH.service
-fi
+  function Create_enable_start_sevice() {
+      echo " Create, enable and start Unit /etc/systemd/system/Auth_Warn_SSH.service"
+       cp /root/vdsetup.2/bin/units_systemd/Auth_Warn_SSH.service /etc/systemd/system/Auth_Warn_SSH.service
+       systemctl daemon-reload
+       systemctl enable Auth_Warn_SSH.service
+       systemctl start Auth_Warn_SSH.service || systemctl restart Auth_Warn_SSH.service
+  }
+
+[[ -e /etc/systemd/system/Auth_Warn_SSH.service ]] && echo " Unit file /etc/systemd/system/Auth_Warn_SSH.service exists." || Create_enable_start_sevice ;
 
 }
 
