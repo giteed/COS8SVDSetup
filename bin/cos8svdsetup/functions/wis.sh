56bac8de19a93a0e3376d8bdbc7c2e16bc943430
giteed <33794654+giteed@users.noreply.github.com>
2023-04-09 20:11:20 +0000

    .

--- bin/cos8svdsetup/functions/wis.sh
+++ bin/cos8svdsetup/functions/wis.sh
@@ -135,7 +135,7 @@ function _locate() {
  | этом сервере, совпадающих с: "$arg_2"
  ⎩ # locate "$arg_2" ") && lang=cr && bpn_p_lang ;	
   echo ;
-  ttb=$(echo -e "$(whatis $arg_2 | column -t | tr -s ' ' )" 2>/dev/null ) && lang=cr && bpn_p_lang ;
+  ttb=$(echo -e "$(whatis $arg_2 | column -t | tr -s ' ' )"  ) 2>/dev/null && lang=cr && bpn_p_lang ;
 	 
   ttb=$(echo -e "$(stat -c '%a:%A %U %G %n' $( (locate "/$arg_2") | (rg "/$arg_2" | rg "/$arg_2") ) 2>/dev/null | column -t ;)\n") && lang=cr && bpal_p_lang;	
 
