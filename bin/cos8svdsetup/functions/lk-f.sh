53d923840e253325b4c25d7cf0c82d65e39c36ea
giteed <33794654+giteed@users.noreply.github.com>
2023-04-09 08:47:00 +0000

    .

--- bin/cos8svdsetup/functions/lk-f.sh
+++ bin/cos8svdsetup/functions/lk-f.sh
@@ -11,7 +11,7 @@ function lk-f()
       then 
       GLIG_ASTRX_OF ;
       ttb=$(echo -e "\n Пустой запрос "lk" покажет все,\n кроме скрытых файлов и папок\n путь:$(pwd)\n") && lang=cr && bpn_p_lang ;
-      ttb=$( stat -c '%a:%A %U %G %s %n' . .. * | numfmt --header --field 4 --from=iec --to=si | column -t ) && lang=java && bpn_p_lang ;
+      ttb=$( echo && stat -c '%a:%A %U %G %s %n' . .. * | numfmt --header --field 4 --from=iec --to=si | column -t ) && lang=java && bpn_p_lang ;
       return; 
    fi
    
@@ -19,13 +19,13 @@ function lk-f()
       then 
       GLIG_ASTRX_OF ;
       ttb=$(echo -e "\n Запрос "lk" c \".\" покажет все,\n включая cкрытые файлы и папки\n путь:$(pwd)\n") && lang=cr && bpn_p_lang ;
-      ttb=$( stat -c '%a:%A %U %G %s %n' .* ** | numfmt --header --field 4 --from=iec --to=si | column -t ) && lang=java && bpn_p_lang ;
+      ttb=$( echo && stat -c '%a:%A %U %G %s %n' .* ** | numfmt --header --field 4 --from=iec --to=si | column -t ) && lang=java && bpn_p_lang ;
       return; 
    fi
    
       GLIG_ASTRX_OF ;
       ttb=$(echo -e "\n Вы можете выводить файлы и папки,\n используя маску. Пример: # lk *e*\n путь:$(pwd) \n") && lang=cr && bpn_p_lang ;
-      ttb=$( stat -c '%a:%A %U %G %s %n' $* | numfmt --header --field 4 --from=iec --to=si | column -t ) && lang=java && bpn_p_lang ;
+      ttb=$( echo && stat -c '%a:%A %U %G %s %n' $* | numfmt --header --field 4 --from=iec --to=si | column -t ) && lang=java && bpn_p_lang ;
 }
 
  #ttb=$(echo -e "\n $(stat -c '%a:%A %U %G %n' $( (locate "/$arg_2") | (rg "/$arg_2" | rg "/$arg_2") ) 2>/dev/null | column -t ;)\n") && lang=cr && bpn_p_lang ;
