dc66bd98b2813615630e5081cd8f99f99cafe9e5
giteed <33794654+giteed@users.noreply.github.com>
2023-04-11 07:43:33 +0000

    .

--- bin/cos8svdsetup/.help/.load_help_func.sh
+++ bin/cos8svdsetup/.help/.load_help_func.sh
@@ -3,4 +3,4 @@
 # --> Прочитать настройки из файла с функциями:
 
 # --> Работа с текстовым процессором bat для подсветки синтаксиса терминала.
-	. /root/vdsetup.2/bin/help/core_grubby_help.sh
+	. /root/vdsetup.2/bin/.help/core_grubby_help.sh
