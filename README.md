7bd6524b86f1c764d45377c66169ce394b53043c
giteed <33794654+giteed@users.noreply.github.com>
2023-04-11 10:14:36 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COS8SVDSetup 
-Version 2.1.803
+Version 2.1.804
 
 ![vdsetup_logo.png](/images/vdsetup_logo.png)
 ## Script for initial preparation of fresh VDS based on CentOS 8 Stream
--- bin/cos8svdsetup/utility/install/vpn/core_ch_for_WireGuard.sh
+++ bin/cos8svdsetup/utility/install/vpn/core_ch_for_WireGuard.sh
@@ -17,13 +17,13 @@ function core_up() {
 
 
 # Выводим описание вывода uname -r
-echo -en " Output of 'uname -r': "
+echo -en "\n Output of 'uname -r': "
 
 # Выводим результат команды uname -r
 uname -r
 
 # Выводим описание вывода ls -l /boot/vmlinuz-*
-echo -e " Output of 'ls -l /boot/vmlinuz-*':"
+echo -e "\n Output of 'ls -l /boot/vmlinuz-*':"
 
 # Выводим результат команды ls -l /boot/vmlinuz-*
 ls -l /boot/vmlinuz-*
@@ -35,7 +35,7 @@ then
   # то выполняем команду fix.sh
   if [ "$(uname -r | cut -d'.' -f1)" -lt 6 ]
   then
-	echo -e " Kernel version is less than 6, running core_grubby_help"
+	echo -e "\n Kernel version is less than 6, running core_grubby_help"
 	core_grubby_help ;
   fi
 else
@@ -43,7 +43,7 @@ else
   # то выполняем команду upcore.sh
   if [ "$(uname -r | cut -d'.' -f1)" -lt 6 ]
   then
-	echo -e " Kernel version is less than 6 and vmlinuz-6.* is not found, running core_up"
+	echo -e "\n Kernel version is less than 6 and vmlinuz-6.* is not found, running core_up"
 	echo -e " Версия ядра "$(uname -r)" не поддерживает WireGuard\n Перед установкой WireGuard вам нужно обновить ядро Linux CentOS." && core_up ; echo ;
   fi
 fi
