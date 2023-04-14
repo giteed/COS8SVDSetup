ed5bbb5511117e9992540b365cf9d2efa5e30998
giteed <33794654+giteed@users.noreply.github.com>
2023-04-14 16:10:29 +0000

    .

--- bin/cos8svdsetup/utility/system/swap_edit.sh
+++ bin/cos8svdsetup/utility/system/swap_edit.sh
@@ -19,7 +19,7 @@ function swap_edit() {
 	CURRENT_SWAP_SIZE=$(test -f /swap* && free -h -t | awk '/^Swap:/ { print $2 }' ) || echo "0 GB"
 	
   echo -e " $(black_U23A7   )	Информация о swap: $CURRENT_SWAP_SIZE" ;
-  echo -en " $(ellow_1       )" ;
+  echo -en " $(ellow_1       )\n" ;
   ( free -h -t | bat  --paging=never -l meminfo -p ) 2>/dev/null || free -h -t  ; echo -en ""
   echo -e "\n $(black_U23A9 ) \n" ;
   
