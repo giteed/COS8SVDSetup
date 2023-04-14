47d60a14d5b036b0063c5afde45d5104aacf9b49
giteed <33794654+giteed@users.noreply.github.com>
2023-04-14 16:27:55 +0000

    .

--- bin/cos8svdsetup/utility/system/swap_edit.sh
+++ bin/cos8svdsetup/utility/system/swap_edit.sh
@@ -72,7 +72,7 @@ function swap_edit() {
 	echo -en " $(white_1    ) $(green_n6). Создаю новый файл /etc/rc.local: " ;
 	( echo_create_rc.local ; ) && green_tick || error_MSG ;
 	echo -e " $(black_1     )"
-	echo -e " $(ellow_1     )    Информация о swap: $CURRENT_SWAP_SIZE \n" ;
+	echo -e " $(ellow_1     )    Информация о swap: ${green}$CURRENT_SWAP_SIZE${nc} \n" ;
 	( swapon | bat --paging=never -l nix -p ; ) 2>/dev/null || swapon ;
 	
 	
