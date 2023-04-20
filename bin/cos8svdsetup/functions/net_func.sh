cdd33e5ff31438c351e52577c82b18dd707b29d1
giteed <33794654+giteed@users.noreply.github.com>
2023-04-20 05:51:45 +0000

    .

--- bin/cos8svdsetup/functions/net_func.sh
+++ bin/cos8svdsetup/functions/net_func.sh
@@ -233,8 +233,21 @@ function fw_i()
    echo -e " ${ELLOW}\n	FirewallD инфо: ${NC}(Открытые ports и services)${NC}" ;
    echo -e "	$(green_tick) $(red_U0023) firewall-cmd --list-all\n" ;
    ( firewall-cmd --list-all | rg "(services|ports)" | rg -v "(forward|source)"  2>/dev/null | bat --paging=never -l nix -p 2>/dev/null ; ) || ( firewall-cmd --list-all | grep -E "(services:|ports:)" | grep -v "(forward|source)" ;)
+   
 }
 
+function fw_i_r()
+{
+   function get-all-rules() {
+       echo -e " \n	FirewallD rules: " ;
+       echo -e " # firewall-cmd --list-all\n" ;
+       echo -e "$( firewall-cmd --list-all | grep -E "(services:|ports:)" | grep -v "(forward|source)" ;)"
+       echo -e "$( sudo firewall-cmd --direct --get-all-rules ;)"
+       ttb=$(get-all-rules) && lang="passwd" && bpn_p_lang ;
+   }
+   
+   
+}
 
 #
 function netstat_i ()
