f687ec1e875eb3876d623dea94c425a7e8a5255f
giteed <33794654+giteed@users.noreply.github.com>
2023-04-21 21:19:00 +0000

    .

--- bin/cos8svdsetup/functions/net_func.sh
+++ bin/cos8svdsetup/functions/net_func.sh
@@ -27,7 +27,7 @@ function check_ip() {
         echo "Не удалось получить IP-адрес, перезапускаю TOR..."
         tor_restart_status ;
     else
-        echo " TOR IP-адрес: $ip\n tor_check_ip_wget"
+        echo -e "\n TOR IP-адрес: $ip\n tor_check_ip_wget"
     fi
 }
 
