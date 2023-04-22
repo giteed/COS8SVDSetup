d4ea28fcfeeeebff55439c4f9b28ddd06aa19012
giteed <33794654+giteed@users.noreply.github.com>
2023-04-22 20:19:08 +0000

    .

--- bin/cos8svdsetup/functions/net_func.sh
+++ bin/cos8svdsetup/functions/net_func.sh
@@ -128,10 +128,10 @@ function check_tor() {
         # Если Tor работает, проверяем, происходит ли через него соединение
         if curl -s --socks5-hostname 127.0.0.1:"${tor_port}" https://check.torproject.org/ | grep -q "This browser is configured to use Tor" ; then
             echo "Соединение через Tor происходит нормально."
-            curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/ | grep -m 1 -E 'Sorry | Congratulations'
+            curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/ | grep -m 1 -E 'Sorry | Congratulations' | sed 's/<[^>]*>//g'
         else
             echo "Соединение не происходит через Tor."
-            curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/ | grep -m 1 -E 'Sorry | Congratulations'
+            curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/ | grep -m 1 -E 'Sorry | Congratulations' | sed 's/<[^>]*>//g'
         fi
     else
         echo "Tor Socks5 не работает."
