124abf6b0759408e748546bdb5cbc9219ccaddbb
giteed <33794654+giteed@users.noreply.github.com>
2023-04-22 20:26:23 +0000

    .

--- bin/cos8svdsetup/functions/net_func.sh
+++ bin/cos8svdsetup/functions/net_func.sh
@@ -114,7 +114,7 @@ function test_tor() {
     # Проверяем доступность onion адреса через Tor
     # | sed 's/<[^>]*>//g', чтобы удалить все теги:
     if curl --socks5-hostname "127.0.0.1:${tor_port}" -s "$onion_addr" | grep -m 1 -E "Browse Privately" | sed 's/<[^>]*>//g' ; then
-        echo "Tor Socks5 работает нормально"
+        echo "Tor Socks5 работает нормально\n Сайт в зоне .onion получен через Socks5 успешно.\n "
     else
         echo "Tor Socks5 не работает"
     fi
