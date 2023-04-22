73a200acd2378f07d34e3c7e1ae3c996d6558016
giteed <33794654+giteed@users.noreply.github.com>
2023-04-22 21:02:15 +0000

    .

--- bin/cos8svdsetup/functions/net_func.sh
+++ bin/cos8svdsetup/functions/net_func.sh
@@ -121,16 +121,15 @@ function test_tor() {
   
 }
 
-tor_onion_test() {
- ttb=$(test_tor ) && lang=cr && bpn_p_lang ;
-}
+function tor_onion_test() { ttb=$(test_tor ) && lang=cr && bpn_p_lang ; }
+
 
 function check_tor() {
     # Сначала проверяем, что Tor запущен и работает как прокси
     if curl -s -x socks5h://127.0.0.1:"${tor_port}" https://check.torproject.org/ | grep -q "Congratulations" ; then
         echo " Tor Socks5 работает нормально."
         # Если Tor работает, проверяем, происходит ли через него соединение
-        if curl -s --socks5-hostname 127.0.0.1:"${tor_port}" https://check.torproject.org/ | grep -q "This browser is configured to use Tor" ; then
+        if curl -s --socks5-hostname 127.0.0.1:"${tor_port}" https://check.torproject.org/ | grep -q " This browser is configured to use Tor" ; then
             echo " Соединение через Tor происходит нормально."
             curl -s --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/ | grep -m 1 -E 'Sorry | Congratulations' | sed 's/  //g'
         else
