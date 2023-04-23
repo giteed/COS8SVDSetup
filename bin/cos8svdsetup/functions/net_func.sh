1e880723c4d8cfb038ca5f5938af2063b8db1c0d
giteed <33794654+giteed@users.noreply.github.com>
2023-04-23 13:26:02 +0000

    .

--- bin/cos8svdsetup/functions/net_func.sh
+++ bin/cos8svdsetup/functions/net_func.sh
@@ -333,29 +333,7 @@ function TopRAM25() {
 function t25r() { TopRAM25 | bat -p -l c }
 
 
-# Функция: 
-function wport() {
-   
-   function netstat_tulanp_nogrep() {
-      ( echo -e "\n${green}$(netstat -tulanp | head --lines 2 | grep -v "Active Internet" )${NC}\n" ; netstat -tulanp | grep -v "Active Internet" | grep -v " Foreign Address" |  bat --paging=never -l nix -p ) || ( echo -e "\n${green}$(netstat -tulanp | head --lines 2 | grep -v "Active Internet" )${NC}\n" ; netstat -tulanp | grep -v "Active Internet" | grep -v " Foreign Address" ) ;
-    }
-   
-   function netstat_tulanp() {
-      
-      ( echo -e "\n${green}$(netstat -tulanp | head --lines 2 | grep -v "Active Internet" )${NC}\n" ; netstat -tulanp | rg "$1" | bat --paging=never -l nix -p ) || ( echo -e "\n${green}$(netstat -tulanp | head --lines 2 | grep -v "Active Internet" )${NC}\n" ; netstat -tulanp | rg $1 ) ;
-    }
-   
-   function help_wport() {
-       echo -e "\n ${green}This function print${NC}:\n  netstat -tulanp | grep \"\$1\"\n\n ${green}Usage${NC}:\n  wport \"keyword\" or \":port\"\n  wport all or \"${red}.${NC}\"\n  wport :22\n  wport :80\n  wport tcp\n  wport udp\n  wport LISTEN\n  wport ESTABLISHED\n  wport 127.0.0.1 "
-       # Usage: grep [OPTION]... PATTERN [FILE]...
-    }
-    if [[ $1 == "" ]] ; then help_wport && return ; fi ;
-    if [[ $1 == "all" ]] ; then netstat_tulanp_nogrep && return ; fi ;
-    if [[ $1 == "." ]] ; then netstat_tulanp_nogrep && return ; fi ;
-    if [[ $1 == "-h" ]] ; then help_wport && return ; fi ;
-    
-    netstat_tulanp $1 ;
-}
+
 
 
 # Функция: Показывает первые 10 прожорливых процессов CPU/RAM
