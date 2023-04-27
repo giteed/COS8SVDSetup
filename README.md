29cd269028b56130af57538529b03d950d0120f5
giteed <33794654+giteed@users.noreply.github.com>
2023-04-27 15:33:38 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COS8SVDSetup 
-Version 2.1.1055
+Version 2.1.1056
 
 ![vdsetup_logo.png](/images/vdsetup_logo.png)
 ## Script for initial preparation of fresh VDS based on CentOS 8 Stream
--- bin/cos8svdsetup/functions/timer.sh
+++ bin/cos8svdsetup/functions/timer.sh
@@ -8,7 +8,7 @@ function show_current_time() {
 
 # Вызов функции-таймера на 5 минут, пример вызова ниже:
 # timer "00:05:00"
-function timer() {
+function timerZ() {
    
    function timer_help() {
 ttb=$( echo -e "
@@ -54,4 +54,55 @@ ttb=$( echo -e "
    
     printf "\nTimer completed!\n"
     
+  }
+
+function timer() {
+   function timer_help() {
+ttb=$( echo -e "
+ Функция timer позволяет назначать время таймера в р
+ азных форматах для удобства использования. 
+ Вот несколько вариантов, как можно указывать время 
+ в функции timer:
+ 1. В секундах: timer \"300\" - таймер на 300 секунд (5 минут).
+ 2. В минутах: timer \"5 minutes\" - таймер на 5 минут.
+ 3. В часах: timer \"1 hour\" - таймер на 1 час.
+ 4. В днях: timer \"2 days\" - таймер на 2 дня.
+ Функция timer использует команду date с опцией -d, которая 
+ поддерживает разные форматы для определения времени. 
+ Вы можете указывать продолжительность таймера в том формате,
+ который вам удобен.
+ Кроме того, вы также можете комбинировать значения времени
+ для создания более сложных интервалов. Например, 
+ timer \"1 hour 30 minutes\" устанавливает таймер на 1 час и 30 минут.
+ Форматы времени должны быть согласованы с командой date 
+ и поддерживаться в вашей операционной системе.
+ ") && lang=nix && bpn_p_lang ;
+    }
+  
+    if [ -z "$1" ]; then
+      timer_help
+      return 1
+    fi
+  
+    if ! duration=$(date -ud "$1" +%s 2>/dev/null); then
+      timer_help
+      return 1
+    fi
+  
+    end_time=$((SECONDS + duration))
+  
+    while true; do
+      current_time=$SECONDS
+      remaining=$((end_time - current_time))
+  
+      if ((remaining <= 0)); then
+        break
+      fi
+  
+      printf "\rTime remaining: %02d:%02d:%02d" \
+        $((remaining/3600)) $((remaining%3600/60)) $((remaining%60))
+      sleep 1
+    done
+  
+    printf "\nTimer completed!\n"
   }
