ba73d8fa3f3ccab5f1439bcc7f4e86a38dcf2ca4
giteed <33794654+giteed@users.noreply.github.com>
2023-04-27 15:03:42 +0000

    .

--- bin/cos8svdsetup/functions/timer.sh
+++ bin/cos8svdsetup/functions/timer.sh
@@ -28,3 +28,22 @@ function timer() {
   printf "\nTimer completed!\n"
 }
 
+timer2() {
+  duration=$1
+  end_time=$(date -ud "+$duration" +%s)
+
+  while true; do
+    current_time=$(date -u +%s)
+    remaining=$((end_time - current_time))
+
+    if ((remaining <= 0)); then
+      break
+    fi
+
+    printf "\rTime remaining: %02d:%02d:%02d" \
+      $((remaining/3600)) $((remaining%3600/60)) $((remaining%60))
+    sleep 1
+  done
+
+  printf "\nTimer completed!\n"
+}
