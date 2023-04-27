7275d43b862b73be2b51c920f7cf0ffb3438de87
giteed <33794654+giteed@users.noreply.github.com>
2023-04-27 17:29:35 +0000

    .

--- bin/cos8svdsetup/utility/install/shredder/shredder.sh
+++ bin/cos8svdsetup/utility/install/shredder/shredder.sh
@@ -182,7 +182,7 @@ function desktop_shredder() {
     
     mkdir -p /root/temp/shredder
     path=$(echo /root/temp/shredder/)
-    ttb=$(echo -e  " \"Desktop Shredder\" скоро начнет очистку\n папки $path") && lang=cr && bpn_p_lang
+    ttb=$(echo -e  "\n \"Desktop Shredder\" скоро начнет очистку\n папки $path") && lang=cr && bpn_p_lang ; echo ;
     timer "5 sec";
     path=$(echo /root/temp/shredder/)
     n=3
@@ -190,7 +190,7 @@ function desktop_shredder() {
     #ttb=$(echo -e  "desktop_shredder path $path") && lang=cr && bpn_p_lang
     cycle_ssl $path $n && cycle_zero $path $n && shred $path $n && deleting_empty_folders $path $n
     
-    ttb=$(echo -e  " \"Desktop Shredder\" старательно измельчил\n все содержимое папки $path") && lang=cr && bpn_p_lang
+    ttb=$(echo -e  "\n \"Desktop Shredder\" старательно измельчил\n все содержимое папки $path") && lang=cr && bpn_p_lang ; echo ;
     
   }
 
