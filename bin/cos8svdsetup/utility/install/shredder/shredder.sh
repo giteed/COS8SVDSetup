6d549d2c763777c4f16545b59fe18edef4ebd1a7
giteed <33794654+giteed@users.noreply.github.com>
2023-04-28 20:37:06 +0000

    .

--- bin/cos8svdsetup/utility/install/shredder/shredder.sh
+++ bin/cos8svdsetup/utility/install/shredder/shredder.sh
@@ -14,7 +14,33 @@ function deleting_empty_zero_folders() {
     
   }
 
-
+  function check_screen_process() {
+    path=$1
+    # Получаем PID текущего процесса
+  
+    local process_name="d_s_h_r_e_d_d_e_r"
+    
+    echo -e "\n "\$process_name d_s_h_r_e_d_d_e_r:" "$process_name" "
+    
+    ttb=$(echo -e "\n Проверяем наличие процесса d_s_h_r_e_d_d_e_r $(ps aux | grep "$process_name" | grep -v grep ) с помощью pgrep \n") && lang=cr && bpn_p_lang
+    
+    
+    
+    # Проверяем наличие процесса с помощью pgrep
+    if ps aux | grep "$process_name" | grep -v grep >/dev/null; 
+      then
+        ttb=$(echo -e "\n Процесс $process_name уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r $process_name") && lang=cr && bpn_p_lang
+        exit 1
+      else
+        ttb=$(echo -e "\n Процесс shredder $process_name не найден в памяти\n можно запускать.\n Проверить процесс: # screen -r ") && lang=cr && bpn_p_lang
+           
+        echo 
+        timer "10 sec";
+        press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
+        
+    fi
+  }
+  
 
 function replace_with_zeros() {
     
@@ -177,6 +203,7 @@ function deleting_empty_folders() {
 # Старт с запросом папки и количества интераций
 function shred_request() {
     
+    check_screen_process ;
     request_path && request_n 
     
     # Путь до рабочей папки с которой производим действия
@@ -216,7 +243,7 @@ function desktop_shredder() {
     ttb=$(echo -en  "\n \"Desktop Shredder\" скоро начнет\n очистку папки: \n") && lang=cr && bpn_p_lang ; 
     tree -aC -L 2 $path ;
     echo ;
-    timer "15 sec";
+    check_screen_process ;
     check_empty_folder ;
     
     deleting_empty_zero_folders $path $n ;
