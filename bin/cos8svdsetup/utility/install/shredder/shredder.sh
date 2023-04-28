0b3784dd3f6ddbf5130352ff42d7ac8f5df5dcfd
giteed <33794654+giteed@users.noreply.github.com>
2023-04-28 13:26:16 +0000

    .

--- bin/cos8svdsetup/utility/install/shredder/shredder.sh
+++ bin/cos8svdsetup/utility/install/shredder/shredder.sh
@@ -15,17 +15,19 @@ function deleting_empty_zero_folders() {
   }
 
 function check_screen_process() {
-    path=$1
-    n=$2
-    local process_name='shredder'
-    ttb=$(echo -e "\n Проверяем наличие процесса $(pgrep -f "$process_name" | grep -v "$$") с помощью pgrep, исключая текущий процесс "$$" \n") && lang=cr && bpn_p_lang
-    timer "10 sec";
-    # Проверяем наличие процесса с помощью pgrep, исключая текущий процесс
-    if pgrep -f "$process_name" | grep -v "$$" >/dev/null; then
-      ttb=$(echo -e "\n Процесс $process_name уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r $process_name") && lang=cr && bpn_p_lang
-      exit 1
-    fi
+      path=$1
+      current_pid=$2
+      local process_name='shredder'
+      
+      # Проверяем наличие процесса с помощью pgrep, исключая текущий процесс
+      if pgrep -f "$process_name" | grep -v "$current_pid" >/dev/null; then
+          ttb=$(echo -e "\n Процесс $process_name уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r $process_name") && lang=cr && bpn_p_lang
+          exit 1
+      fi
   }
+  
+
+
 
 
 
@@ -227,7 +229,14 @@ function desktop_shredder() {
     
     # Проверяем наличие процесса с помощью screen -list
     deleting_empty_zero_folders $path $n ; 
-    check_screen_process $path $n ;
+    #check_screen_process $path $n ;
+    # Получаем PID текущего процесса
+    current_pid=$$
+    
+    # Вызываем функцию check_screen_process с передачей пути и текущего PID
+    check_screen_process "$path" "$current_pid"
+    
+    
     
     ttb=$(echo -en  "\n \"Desktop Shredder\" скоро начнет\n очистку папки: \n") && lang=cr && bpn_p_lang ; 
     tree -aC -L 2 $path ;
