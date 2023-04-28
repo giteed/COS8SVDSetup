b701fbcc9cfb9dffa5f30a1ab882c23a09257e54
giteed <33794654+giteed@users.noreply.github.com>
2023-04-28 06:21:17 +0000

    .

--- bin/cos8svdsetup/utility/install/shredder/shredder.sh
+++ bin/cos8svdsetup/utility/install/shredder/shredder.sh
@@ -15,10 +15,14 @@ function deleting_empty_zero_folders() {
   }
 
 function check_screen_process() {
-    local process_name='*.shredder'
+    path=$1
+    n=$2
+    #ttb=$(echo -e  "check_screen_process n= $n") && lang=cr && bpn_p_lang
+    #ttb=$(echo -e  "check_screen_process path $path") && lang=cr && bpn_p_lang
+    local process_name='SCREEN -dmS shredder'
     
     # Проверяем наличие процесса с помощью screen -list
-    if sudo screen -list | grep -Eq '$process_name'; then
+    if ps aux | grep -E '$process_name' | grep -v 'grep'; then
         ttb=$(echo -e "\n Процесс $process_name уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r $process_name") && lang=cr && bpn_p_lang
         exit 1 ;
     fi
@@ -26,8 +30,8 @@ function check_screen_process() {
 }
 
 # Проверяем наличие процесса с помощью screen -list
-deleting_empty_zero_folders ;
-check_screen_process ;
+deleting_empty_zero_folders $path $n ; ;
+check_screen_process $path $n;
 
 
 function replace_with_zeros() {
