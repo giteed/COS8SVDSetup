2d36b3bb0863e893c6ad4a4f8c25cc612e52ef49
giteed <33794654+giteed@users.noreply.github.com>
2023-04-28 19:29:39 +0000

    .

--- bin/cos8svdsetup/utility/install/shredder/help-shr.sh
+++ bin/cos8svdsetup/utility/install/shredder/help-shr.sh
@@ -9,21 +9,21 @@
     path=$1
     # Получаем PID текущего процесса
 
-    local process_name='shredder'
+    local process_name='-dmS shredder'
     
     echo -e " "shredder-\$process_name :" "$process_name" "
     
-    ttb=$(echo -e "\n Проверяем наличие процесса shredder $(ps aux | grep "$process_name" | grep -v grep ) с помощью grep \n") && lang=cr && bpn_p_lang
+    ttb=$(echo -e "\n Проверяем наличие процесса shredder $(ps aux | grep '$process_name' | grep -v 'grep' ) с помощью grep \n") && lang=cr && bpn_p_lang
     
     
     
     # Проверяем наличие процесса с помощью grep
-    if ps aux | grep "$process_name" | grep -v grep >/dev/null; 
+    if ps aux | grep '$process_name' | grep -v grep >/dev/null; 
       then
-        ttb=$(echo -e "\n Процесс $process_name уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r $process_name") && lang=cr && bpn_p_lang
+        ttb=$(echo -e "\n Процесс '$process_name' уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r '$process_name' ") && lang=cr && bpn_p_lang
         exit 1
       else
-        ttb=$(echo -e "\n Процесс shredder $process_name не найден в памяти\n можно запускать.\n Проверить процесс: # screen -r $process_name") && lang=cr && bpn_p_lang
+        ttb=$(echo -e "\n Процесс shredder '$process_name' не найден в памяти\n можно запускать.\n Проверить процесс: # screen -r '$process_name' ") && lang=cr && bpn_p_lang
            
         echo 
         timer "30 sec";
