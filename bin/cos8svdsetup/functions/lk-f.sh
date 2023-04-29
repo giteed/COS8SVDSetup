1d9849c70717088309be2e7e21d2a4b8b28fc13b
giteed <33794654+giteed@users.noreply.github.com>
2023-04-29 14:24:03 +0000

    .

--- bin/cos8svdsetup/functions/lk-f.sh
+++ bin/cos8svdsetup/functions/lk-f.sh
@@ -30,5 +30,5 @@ function lk-f() {
  #ttb=$(echo -e "\n $(stat -c '%a:%A %U %G %n' $( (locate "/$arg_2") | (rg "/$arg_2" | rg "/$arg_2") ) 2>/dev/null | column -t ;)\n") && lang=cr && bpn_p_lang ;
  
  function lk() {
-    ('GLIG_ASTRX_OF && lk-f')
+    ('GLIG_ASTRX_OF && lk-f') 2>/dev/null ;
    }
