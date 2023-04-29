2eb6b92bdd651035db6bf514e8eef07e9514d131
giteed <33794654+giteed@users.noreply.github.com>
2023-04-29 18:05:04 +0000

    .

--- bin/cos8svdsetup/functions/other.sh
+++ bin/cos8svdsetup/functions/other.sh
@@ -27,6 +27,11 @@ function mvds() {
 			
 		}
 		
-		_mvds $1
-		lk $ds_path ;
+		_mvds $1 ;
+		cur_path=pwd ;
+		cd $ds_path && lk ;
+		cd cur_path ;
+		
+		ttb=$(echo -e  "\n \"Desktop Shredder\" \n скоро начнет очистку этой папки: $ds_path\n  \n systemctl stop desktop_shredder.service для отмены.") && lang=cr && bpn_p_lang ; echo ;
+		
 	}
