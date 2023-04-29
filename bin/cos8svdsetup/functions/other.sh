88d9ce26061d63bbf82d7ae1872d2546404ab563
giteed <33794654+giteed@users.noreply.github.com>
2023-04-29 15:06:47 +0000

    .

--- bin/cos8svdsetup/functions/other.sh
+++ bin/cos8svdsetup/functions/other.sh
@@ -14,3 +14,19 @@ function im() { whoami ; } ;
 
 # Поиск программ, по 6 утилитам
 function wis() { (GLIG_ASTRX_ON && wis-f $1 $2) } ;
+
+
+# перемещение папки или файла в папку Desktop Shredder
+function mvds() {
+		
+		function _mvds() {
+			# Путь до рабочей папки с которой производим действия
+			ds_path=$(cat /tmp/Desktop_Shredder_path.txt)
+			# Перемещение выбранной файла или папки в папку Desktop Shredder для последующего измельчения
+			mv $1 $ds_path ;
+			
+		}
+		
+		_mvds $1
+		lk $ds_path ;
+	}
