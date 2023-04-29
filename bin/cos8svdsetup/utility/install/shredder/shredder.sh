88633f5f11f629e31a2ae3d8be20f09f746a4b58
giteed <33794654+giteed@users.noreply.github.com>
2023-04-29 10:50:55 +0000

    .

--- bin/cos8svdsetup/utility/install/shredder/shredder.sh
+++ bin/cos8svdsetup/utility/install/shredder/shredder.sh
@@ -5,33 +5,34 @@
 . /root/.bashrc
 
 function deleting_empty_zero_folders() {
-    path=$1
-    n=$2
-    # команда find будет искать папки в указанном пути ($path), фильтровать папки, в названии которых содержатся только нули (-regex '.*/0+$'), и выбирать только пустые папки (-empty). Затем найденные папки будут удалены (-delete).
-    find "$path" -mindepth 1 -type d -regex '.*/0+$' -empty -delete ;
+    sw_path=$1
+    iteration_n=$2
+    # команда find будет искать папки в указанном пути ($sw_path), фильтровать папки, в названии которых содержатся только нули (-regex '.*/0+$'), и выбирать только пустые папки (-empty). Затем найденные папки будут удалены (-delete).
+    find "$sw_path" -mindepth 1 -type d -regex '.*/0+$' -empty -delete ;
     
   }
 
   function check_screen_process() {
     #sleep 7
-    path=$1
+    sw_path=$1
     # Получаем имя текущего процесса
     local screen_name="d_s_h_r_e_d_d_e_r"
-    local pr_n1="shred"
-    local pr_n2="find /root/temp/shredder/"
-    local pr_n3="rm"
-    local pr_n4="mv"
+    local check_1="shred"
+    local check_2="find /root/temp/shredder/"
+    local check_3="rm"
+    local check_4="mv"
     
     echo ;
     ttb=$(echo -e "\n Проверяем наличие процессов с помощью pgrep") && lang=cr && bpn_p_lang ; echo ;
     
-    ps aux | (pgrep -x "$pr_n1" || pgrep -x "$pr_n2"|| pgrep -x "$pr_n3"|| pgrep -x "$pr_n4")
+    ps aux | (pgrep -x "$check_1" || pgrep -x "$check_2"|| pgrep -x "$check_3"|| pgrep -x "$check_4")
+    
     echo ;
     # Проверяем наличие процесса с помощью pgrep
-    if ps aux | (pgrep -x "$pr_n1" || pgrep -x "$pr_n2"|| pgrep -x "$pr_n3"|| pgrep -x "$pr_n4") >/dev/null; 
+    if ps aux | (pgrep -x "$check_1" || pgrep -x "$check_2"|| pgrep -x "$check_3"|| pgrep -x "$check_4") >/dev/null; 
       then
         ttb=$(echo -e "\n Процесс $screen_name уже запущен.\n Дождитесь завершения работы Shredder.\n Проверить процесс: # screen -r $screen_name") && lang=cr && bpn_p_lang
-        deleting_empty_zero_folders $path $n ;
+        deleting_empty_zero_folders $sw_path $iteration_n ;
         #timer "10 sec"
         exit 1
       else
@@ -39,7 +40,7 @@ function deleting_empty_zero_folders() {
            
         echo ;
         #timer "10 sec";
-        deleting_empty_zero_folders $path $n ;
+        deleting_empty_zero_folders $sw_path $iteration_n ;
         #press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
         return ;
     fi
@@ -47,13 +48,13 @@ function deleting_empty_zero_folders() {
   
 function replace_folder_name_with_zeros() {
     
-    path=$1
-    n=$2
+    sw_path=$1
+    iteration_n=$2
     # Считает количество символов в названии переданной на вход папки 
     # от переменной folder_name и отдает столько-же нулей на выход
    function _replace_folder_name_with_zeros() {
-     path=$1
-     name=$(basename "$path")
+     sw_path=$1
+     name=$(basename "$sw_path")
      num_chars=${#name}
      zero_string=$(printf '%*s' "$num_chars" | tr ' ' '0')
      echo "$zero_string"
@@ -61,7 +62,7 @@ function replace_folder_name_with_zeros() {
     
     # Получаем список папок внутри заданного пути, отсортированных по глубине вложенности
     # Передаем список папок через конвейер в awk для обработки
-    find "$path" -mindepth 1 -type d | awk -F/ 'NF{print NF-1,$0}' | sort -nr | cut -d" " -f2- | while read folder_name; 
+    find "$sw_path" -mindepth 1 -type d | awk -F/ 'NF{print NF-1,$0}' | sort -nr | cut -d" " -f2- | while read folder_name; 
     do
       
       # Генерируем новое имя для папки из нулей
@@ -86,19 +87,19 @@ function replace_folder_name_with_zeros() {
 
 function shred_files() {
     
-    path=$1
-    n=$2
+    sw_path=$1
+    iteration_n=$2
     # Удаляем все файлы в указанной директории и ее поддиректориях
-    find "$path" -type f -exec shred -n 1 -f -u -v -z {} \;
+    find "$sw_path" -type f -exec shred -n 1 -f -u -v -z {} \;
   }
 
 function rename_folder_name_with_openssl_hex() {
     
-    path=$1
-    n=$2
+    sw_path=$1
+    iteration_n=$2
      # Получаем список папок внутри заданного пути, отсортированных по глубине вложенности
      # Передаем список папок через конвейер в awk для обработки
-     find "$path" -mindepth 1 -type d | awk -F/ 'NF{print NF-1,$0}' | sort -nr | cut -d" " -f2- | while read folder; 
+     find "$sw_path" -mindepth 1 -type d | awk -F/ 'NF{print NF-1,$0}' | sort -nr | cut -d" " -f2- | while read folder; 
       do
         # Генерируем случайное имя для новой папки утилитой openssl
         new_name="${folder%/*}/$(openssl rand -hex 3)"
@@ -116,12 +117,12 @@ function rename_folder_name_with_openssl_hex() {
 
 function cycle_openssl_hex() {
     
-    path=$1
-    n=$2
+    sw_path=$1
+    iteration_n=$2
     # Цикл, который выполняется n раз
-    for (( i=1; i<=$n; i++ ))
+    for (( i=1; i<=$iteration_n; i++ ))
       do
-        rename_folder_name_with_openssl_hex $path $n ;
+        rename_folder_name_with_openssl_hex $sw_path $iteration_n ;
         ttb=$(echo -e " Выполнение cycle_openssl_hex  номер $i") && lang=cr && bpn_p_lang
     done
     
@@ -129,39 +130,39 @@ function cycle_openssl_hex() {
 
 function cycle_zero() {
    
-   path=$1
-   #n=$2
-   n=1
+   sw_path=$1
+   #iteration_n=$2
+   iteration_n=1
     # Цикл, который выполняется n раз
-    for (( i=1; i<=$n; i++ ))
+    for (( i=1; i<=$iteration_n; i++ ))
       do
-        replace_folder_name_with_zeros $path $n ;
+        replace_folder_name_with_zeros $sw_path $iteration_n ;
         ttb=$(echo -e " Выполнение cycle_zero номер $i") && lang=cr && bpn_p_lang
     done
     
   }
 
-function request_path() {
+function request_sw_path() {
     
     while true; do
-      read -p " Введите путь до папки: " path
-       if [ ! -d "$path" ]; then
+      read -p " Введите путь до папки: " sw_path
+       if [ ! -d "$sw_path" ]; then
           ttb=$(echo -e " Папка не найдена. Попробуйте еще раз.") && lang=cr && bpn_p_lang
         else
-          echo $path > /tmp/shredder_request_path.txt
+          echo $sw_path > /tmp/shredder_request_sw_path.txt
           break
        fi
     done
     
   }
 
-function request_n() {
+function request_iteration_n() {
     
     while true; do
-      read -p " Введите количество выполнений цикла: " n
-       if [[ ! "$n" =~ ^[0-9]+$ ]]; then
+      read -p " Введите количество выполнений цикла: " iteration_n
+       if [[ ! "$iteration_n" =~ ^[0-9]+$ ]]; then
           ttb=$(echo -e " Неверный формат. Введите только цифры.") && lang=cr && bpn_p_lang
-        elif [ "$n" -eq 0 ]; then
+        elif [ "$iteration_n" -eq 0 ]; then
           ttb=$(echo -e " Количество выполнений должно быть больше 0.") && lang=cr && bpn_p_lang
         else
           break
@@ -172,26 +173,26 @@ function request_n() {
 
 function deleting_empty_folders() {
     
-    path=$1
-    n=$2
+    sw_path=$1
+    iteration_n=$2
     # Удаляем пустые директории кроме родительской папки
-    rm -rf $(find "$path" -mindepth 1 -type d | awk -F/ 'NF{print NF-1,$0}' | sort -nr | cut -d" " -f2-)
-    tree -aC -L 2 $path ;
-    #find "$path" -mindepth 1 -type d -regex '.*/0+$' -empty -delete
+    rm -rf $(find "$sw_path" -mindepth 1 -type d | awk -F/ 'NF{print NF-1,$0}' | sort -nr | cut -d" " -f2-)
+    tree -aC -L 2 $sw_path ;
+    #find "$sw_path" -mindepth 1 -type d -regex '.*/0+$' -empty -delete
   }
 
 # Старт с запросом папки и количества интераций
 function shred_request() {
     tstart ;
     check_screen_process ;
-    request_path && request_n 
+    request_sw_path && request_iteration_n 
     
     # Путь до рабочей папки с которой производим действия
-    path=$(cat /tmp/shredder_request_path.txt)
+    sw_path=$(cat /tmp/shredder_request_sw_path.txt)
     echo ;
-    tree -aC -L 2 $path ; echo ; timer 5 sec ; tstart
-    deleting_empty_zero_folders $path $n ;
-    shred_files $path $n && cycle_openssl_hex $path $n && cycle_zero $path $n && deleting_empty_folders $path $n ;
+    tree -aC -L 2 $sw_path ; echo ; timer 5 sec ; tstart
+    deleting_empty_zero_folders $sw_path $iteration_n ;
+    shred_files $sw_path $iteration_n && cycle_openssl_hex $sw_path $iteration_n && cycle_zero $sw_path $iteration_n && deleting_empty_folders $sw_path $iteration_n ;
     
     ttb=$(echo -e  " Готово \"shred_request\" !") && lang=cr && bpn_p_lang
     tendl ;
@@ -200,11 +201,11 @@ function shred_request() {
 function desktop_shredder() {
     
     mkdir -p /root/temp/shredder
-    path="/root/temp/shredder/"
-    n=1 # Количество интераций (проходов)
+    sw_path="/root/temp/shredder/"
+    iteration_n=1 # Количество интераций (проходов)
     
     check_empty_folder() {
-      shredder_folder="$path"
+      shredder_folder="$sw_path"
      
       if [ -z "$(ls -A "$shredder_folder")" ]; then
         echo -en "\n Папка Desktop Shredder, пуста.\n Нечего мельчить!\n Выход.\n"
@@ -215,16 +216,16 @@ function desktop_shredder() {
      
     
     ttb=$(echo -en  "\n \"Desktop Shredder\" скоро начнет\n очистку папки: \n") && lang=cr && bpn_p_lang ; 
-    #tree -aC -L 2 $path ;
+    #tree -aC -L 2 $sw_path ;
     echo ;
-    check_screen_process $path $n ;
+    check_screen_process $sw_path $iteration_n ;
     tstart ;
-    check_empty_folder $path $n;
+    check_empty_folder $sw_path $iteration_n;
     
-    deleting_empty_zero_folders $path $n ;
-    shred_files $path $n && cycle_openssl_hex $path $n && cycle_zero $path $n && deleting_empty_folders $path $n
+    deleting_empty_zero_folders $sw_path $iteration_n ;
+    shred_files $sw_path $iteration_n && cycle_openssl_hex $sw_path $iteration_n && cycle_zero $sw_path $iteration_n && deleting_empty_folders $sw_path $iteration_n
     
-    ttb=$(echo -e  "\n \"Desktop Shredder\" старательно измельчил\n все содержимое папки: $path") && lang=cr && bpn_p_lang ; echo ; tendl ;
+    ttb=$(echo -e  "\n \"Desktop Shredder\" старательно измельчил\n все содержимое папки: $sw_path") && lang=cr && bpn_p_lang ; echo ; tendl ;
     
   }
 
@@ -243,3 +244,6 @@ function desktop_shredder() {
 
 exit 0
 
+# Функции отсчета времени начала и конца выполнения скрипта
+# tstart # начало 
+# tendl # конец
