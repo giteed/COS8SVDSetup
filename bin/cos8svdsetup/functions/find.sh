628a54638db9935b9bfd694d6731ea7d1464946d
giteed <33794654+giteed@users.noreply.github.com>
2023-05-02 22:42:29 +0000

    .

--- bin/cos8svdsetup/functions/find.sh
+++ bin/cos8svdsetup/functions/find.sh
@@ -226,12 +226,12 @@ sis() {
        fi
    }
    
-   ttb=$(help_sis) && lang=cr && bpn_p_lang
+   
   
       pattern="$1"
       directory="${2:-/}"  # Если путь не указан, то используется корневая директория /
       filetype="*.sh"
-  
+  ttb=$(help_sis) && lang=cr && bpn_p_lang
       grep -rl "$pattern" --include "$filetype" "$directory"
   }
 
