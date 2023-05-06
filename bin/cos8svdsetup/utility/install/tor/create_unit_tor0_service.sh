1c89251a1e1dd3d7eeeb36e51c6e2659edae5a07
giteed <33794654+giteed@users.noreply.github.com>
2023-05-06 22:44:15 +0000

    .

--- bin/cos8svdsetup/utility/install/tor/create_unit_tor0_service.sh
+++ bin/cos8svdsetup/utility/install/tor/create_unit_tor0_service.sh
@@ -80,7 +80,7 @@ EOF
       ttb=$(echo -e "\n The desktop_shredder.service unit was successfully created and started.\n\n To set a different Interface Name for a unit,\n enter # tor_Interface_unit_reinstall [intrface_name]\ For example: # tor_Interface_unit_reinstall tor1\n View Status of TOR service unit # status_tor_service" ) && lang=cr && bpn_p_lang ;
       echo ;
       
-      status_tor_service ; 
+      status_tor_service ; echo ;
       ttb=$(echo -e "$(ifconfig tor0)") && lang=cr && bpn_p_lang ;
     
   }
