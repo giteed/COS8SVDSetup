bba293e5b3c1510faef048f0583a52ba452ced8a
giteed <33794654+giteed@users.noreply.github.com>
2023-05-07 00:48:15 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COS8SVDSetup 
-Version 2.1.1421
+Version 2.1.1422
 
 ![vdsetup_logo.png](/images/vdsetup_logo.png)
 ## Script for initial preparation of fresh VDS based on CentOS 8 Stream
--- bin/cos8svdsetup/utility/install/tor/create_unit_tor0_service.sh
+++ bin/cos8svdsetup/utility/install/tor/create_unit_tor0_service.sh
@@ -7,7 +7,7 @@
 # --> Эта ссылка на функцию проверяет, запущен-ли скрипт с правами суперпользователя (root) в Linux.
 . /root/vdsetup.2/bin/functions/run_as_root.sh ;
 
-
+echo ;
 intrface_name="$1"
 #echo -e " Interface Name unit до условия = $intrface_name"
 
@@ -36,29 +36,30 @@ ip_mask="$(ip -o -f inet addr show | grep "$intrface_name" | awk '{print $4}')"
 unit_file="/etc/systemd/system/tor0.service"
 
 # Проверка наличия файла юнита
-if [ -f "$unit_file" ]; 
-        then
-            echo ;
-            (ttb=$(echo -e "$(ifconfig "$intrface_name")") && lang=cr && bpn_p_lang ;) 2>/dev/null ;
-            
-            ttb=$(echo -e "\n The "$intrface_name".service unit already exists.\n Remove it?") && lang=cr && bpn_p_lang ;
-             echo ;
-            ttb=$(echo -e " This command will remove the "$intrface_name" interface from the system:\n # sudo ip link delete "$intrface_name" ") && lang=cr && bpn_p_lang ;
-            ttb=$(echo -e " This command will turn off the "$intrface_name" interface in the system:\n # sudo ip link set dev $intrface_name down ") && lang=cr && bpn_p_lang ;
-            ttb=$(echo -e " You can delete the current IP address of the "$intrface_name" interface using the command:\n # sudo ip addr del "$ip_mask" dev "$intrface_name" ") && lang=cr && bpn_p_lang ;
-            ttb=$(echo -e " And then add another new IP address using the command:\n # sudo ip addr add 10.0.1.1/24 dev tor0 ") && lang=cr && bpn_p_lang ;
-            ttb=$(echo -e "  ") && lang=cr && bpn_p_lang ;
-            
-            press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
-        # Выключение и удаление старого юнита из systemd
-            systemctl disable tor0.service 2>/dev/null ;
-            systemctl stop tor0.service ;
-        # Перезагрузка конфигурации юнитов
-            systemctl daemon-reload
-        # Удаление файла старого юнита
-            rm /etc/systemd/system/tor0.service ;
-        $0 "$intrface_name"
+if [ -f "$unit_file" ];  then
+      
+     (ttb=$(echo -e "$(ifconfig "$intrface_name")") && lang=cr && bpn_p_lang ;) 2>/dev/null ;
+     
+     ttb=$(echo -e "\n The "$intrface_name".service unit already exists.\n Remove it?") && lang=cr && bpn_p_lang ;
+      echo ;
+     ttb=$(echo -e " This command will remove the "$intrface_name" interface from the system:\n # sudo ip link delete "$intrface_name" ") && lang=cr && bpn_p_lang ;
+     ttb=$(echo -e " This command will turn off the "$intrface_name" interface in the system:\n # sudo ip link set dev $intrface_name down ") && lang=cr && bpn_p_lang ;
+     ttb=$(echo -e " You can delete the current IP address of the "$intrface_name" interface using the command:\n # sudo ip addr del "$ip_mask" dev "$intrface_name" ") && lang=cr && bpn_p_lang ;
+     ttb=$(echo -e " And then add another new IP address using the command:\n # sudo ip addr add 10.0.1.1/24 dev tor0 ") && lang=cr && bpn_p_lang ;
+     ttb=$(echo -e "  ") && lang=cr && bpn_p_lang ;
+     
+     press_enter_to_continue_or_ESC_or_any_key_to_cancel ;
+    # Выключение и удаление старого юнита из systemd
+     systemctl disable tor0.service 2>/dev/null ;
+     systemctl stop tor0.service ;
+    # Перезагрузка конфигурации юнитов
+     systemctl daemon-reload
+    # Удаление файла старого юнита
+     rm /etc/systemd/system/tor0.service ;
+     
+     $0 "$intrface_name"
      exit 1
+     
 fi
 
 intrface_name="$1"
