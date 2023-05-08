5b2a44d82d52f6c3e7246911d5b780eb5cf8b8f3
giteed <33794654+giteed@users.noreply.github.com>
2023-05-08 15:43:49 +0000

    .

--- bin/cos8svdsetup/utility/install/epel_repo_pack.sh
+++ bin/cos8svdsetup/utility/install/epel_repo_pack.sh
@@ -31,7 +31,8 @@
 	 
 	 
 	 sudo dnf install -y epel-release yum-utils npm || ( error_MSG ; ) ; echo ;
-	 sudo dnf install -y net-tools tcpdump bind-utils network-scripts iptables socat dnstracer nyx || ( error_MSG ; ) ; echo ;
+	 sudo dnf install -y network-scripts iptables socat dnstracer nyx || ( error_MSG ; ) ; echo ;
+	 sudo dnf install -y net-tools tcpdump  bind-utils traceroute || ( error_MSG ; ) ; echo ;
 	 sudo dnf install -y dialog mlocate ncdu ranger tldr || ( error_MSG ; ) ; echo ;
 	 sudo dnf install -y git tar curl wget jq nc || ( error_MSG ; ) ; echo ;
 	 sudo dnf install -y whois || ( error_MSG ; ) ; echo ;
@@ -77,7 +78,7 @@
  ⎧ Установка дополнительных пакетов:: 
  | 
  | epel-release, iptables, nyx, python3, ruby, npm, unzip, 
- | hstr, lsof, screen, tar, p7zip, mc, nano, whois, Webmin,
+ | hstr, lsof, screen, tar, p7zip, mc, nano, whois, Webmin, traceroute,
  | wget, curl, jq, nc, atop, htop, nethogs, bpytop, iftop, bat,
  | stacer, yum-utils, net-tools, network-scripts, git, tree,
  | dialog, mlocate qrencode, tcpdump, ncdu, ranger, tldr, whois, 
