ed8a8115292d7470b7dd9b4f807a63dfd73fa828
giteed <33794654+giteed@users.noreply.github.com>
2023-05-14 17:32:27 +0000

    .

--- bin/cos8svdsetup/utility/install/epel_repo_pack.sh
+++ bin/cos8svdsetup/utility/install/epel_repo_pack.sh
@@ -19,7 +19,7 @@
 			sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 			# Установите репозиторий ELRepo:
 			sudo rpm -Uvh https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
-			sudo dnf install kernel-ml wireguard-tools
+			sudo dnf install wireguard-tools
 	    }
 	 
 		function ffmpeg_install() {
