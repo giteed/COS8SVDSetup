35940f1f2a9ff462ab4dd7bba04cff0389714499
giteed <33794654+giteed@users.noreply.github.com>
2023-05-14 17:47:16 +0000

    .

--- bin/cos8svdsetup/utility/install/epel_repo_pack.sh
+++ bin/cos8svdsetup/utility/install/epel_repo_pack.sh
@@ -33,7 +33,7 @@
 	 sudo dnf install -y epel-release yum-utils npm || ( error_MSG ; ) ; echo ;
 	 sudo dnf install -y network-scripts iptables socat dnstracer nyx || ( error_MSG ; ) ; echo ;
 	 sudo dnf install -y net-tools tcpdump  bind-utils traceroute || ( error_MSG ; ) ; echo ;
-	 sudo dnf install -y hydra libssh2-devel nmap || ( error_MSG ; ) ; echo ;
+	 sudo dnf install -y hydra libssh2-devel nmap lua || ( error_MSG ; ) ; echo ;
 	 #sudo dnf install -y 
 	 sudo dnf install -y dialog mlocate ncdu ranger tldr || ( error_MSG ; ) ; echo ;
 	 sudo dnf install -y git tar curl wget jq nc || ( error_MSG ; ) ; echo ;
@@ -82,7 +82,7 @@
  | epel-release, iptables, nyx, python3, ruby, npm, unzip, libssh2-devel,
  | hstr, lsof, screen, tar, p7zip, mc, nano, whois, Webmin, traceroute,
  | wget, curl, jq, nc, atop, htop, nethogs, bpytop, iftop, bat, hydra,
- | stacer, yum-utils, net-tools, network-scripts, git, tree, nmap,
+ | stacer, yum-utils, net-tools, network-scripts, git, tree, nmap, lua,
  | dialog, mlocate qrencode, tcpdump, ncdu, ranger, tldr, whois, 
  ⎩ youtube-dl, ffmpeg. \n" ) && bpn_p_lang ; echo ;
 	
