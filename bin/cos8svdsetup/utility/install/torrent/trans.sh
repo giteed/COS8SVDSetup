d5542b1f7ec82da5087b1759d440858c5199380a
SERGE <serge@macbook-pro-serge-2.local>
2023-06-29 16:59:32 +0300

    .

--- bin/cos8svdsetup/utility/install/torrent/trans.sh
+++ bin/cos8svdsetup/utility/install/torrent/trans.sh
@@ -280,11 +280,7 @@ echo -e "\n ⎩" ; sleep 1 ;
 		  ttb=$(firewall-cmd --permanent --zone=public --add-port=9091/tcp) && lang="nix" && bpn_p_lang
 		  ttb=$(firewall-cmd --complete-reload) && lang="nix" && bpn_p_lang
 		  ttb=$(firewall-cmd --list-all) && lang="nix" && bpn_p_lang
-<<<<<<< HEAD
 		  echo -e " wg-quick так-же был перезапущен, переподключитесь к VPN WG если вы были подключены к нему на этом сервере."
-=======
-		  echo -e " wg-quick так-же был перезапущен, переподключитесь к VPN WG если вы были подключены к нему на этом сервере"
->>>>>>> main
 		  systemctl restart wg-quick@*.service ;
 		  echo -e "\n ⎩ \n"
 	  }
@@ -295,11 +291,7 @@ function remove_port_9091_and_reload_firewalld() {
 		  ttb=$(firewall-cmd --permanent --zone=public --remove-port=9091/tcp) && lang="nix" && bpn_p_lang
 		  ttb=$(firewall-cmd --complete-reload) && lang="nix" && bpn_p_lang
 		  ttb=$(firewall-cmd --list-all) && lang="nix" && bpn_p_lang
-<<<<<<< HEAD
 		  echo -e " wg-quick так-же был перезапущен, переподключитесь к VPN WG если вы были подключены к нему на этом сервере."
-=======
-		  echo -e " wg-quick так-же был перезапущен, переподключитесь к VPN WG если вы были подключены к нему на этом сервере"
->>>>>>> main
 		  systemctl restart wg-quick@*.service ;
 		  echo -e "\n ⎩ \n"
 		  return ;
