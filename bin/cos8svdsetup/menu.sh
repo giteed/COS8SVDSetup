962dac1bff23b58c5e7dc5eafcd45fd0cc9703f3
SERGE <serge@macbook-pro-serge-2.local>
2023-08-06 17:17:09 +0300

    .

--- bin/cos8svdsetup/menu.sh
+++ bin/cos8svdsetup/menu.sh
@@ -16,7 +16,8 @@ function menu_vdsetup() {
 print_menu() {
 	
 	echo -e
-	echo -e " Выберите опцию:"\n
+	echo -e " Выберите опцию:"
+	echo -e
 	echo -e " 1. Пакет программ и репозиториев для удобства работы с сервером."
 	echo -e
 	echo -e " 2. Создание Unit для Shredder Desktop, автоматический запуск. "
