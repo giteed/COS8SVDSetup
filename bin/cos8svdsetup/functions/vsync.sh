45311e292693a288e0ed9a2938f78ea48a063330
SERGE <serge@macbook-pro-serge-2.local>
2023-08-06 21:38:55 +0300

    .

--- bin/cos8svdsetup/functions/vsync.sh
+++ bin/cos8svdsetup/functions/vsync.sh
@@ -44,7 +44,7 @@ function vsync() {
         cd ~
 # --> Наконец, запускается алиас чтения .bashrc для применения изменений alias urc='source /root/.bashrc'
         . /root/.bashrc
-        urc 2>/dev/null || (urc-f && echo "  Не обновлено, введите в консоль urc или source /root/.bashrc") ;
+        urc 2>/dev/null || (urc-f && echo " Настройки из .bashrc Не обновлены! Введите в консоль urc или source /root/.bashrc") ;
         
         ttb=$(echo -e "  VDSetup $(cat /root/vdsetup.2/bin/VERSION)\n\n  If you need to completely remove,\n  and reinstall vdsetup from GitHub,  type: # rm_clone \n  If you only need to remove vdsetup, type: # rm_vdsetup \n  Alias 'urc' For update .bashrc, or  type: # source /root/.bashrc") && lang_cr ; bpn_p_lang ; 
         echo .......................................................................... ;
