97436ab2c7b6452bc46b3da6d749faeff735661a
SERGE <serge@macbook-pro-serge-2.local>
2023-08-06 14:41:23 +0300

    .

--- bin/cos8svdsetup/menu.sh
+++ bin/cos8svdsetup/menu.sh
@@ -17,20 +17,20 @@ function menu_vdsetup() {
 
 # Функция для печати меню
 print_menu() {
-	echo " Выберите опцию:"
-	echo " 1. Пакет программ и репозиториев для удобства работы с сервером"
-	echo " 2. Создание unit для Shredder Desktop, автоматический запуск. (Посмотреть статус: # dsus)"
-	echo " 3. Установка и управление WireGuard"
-	echo " 4. Установка и управление OpenVPN"
-	echo " 5. Установка и управление Transmission (# transmission / удаление # tr_rm)"
-	echo " -- Забрать загруженные файлы с торрентов # start_light_server"
-	echo " -- Остановить HTTP доступ к папке Downloads # stop_light_server"
-	echo " 6. Проверка обновлений или установка GitHub"
-	echo " 7. Опция 3"
-	echo " 8. Опция 3"
-	echo " 9. Опция 3"
-	echo " 0. Выход"
-	echo
+	echo -e " Выберите опцию:"
+	echo -e " 1. Пакет программ и репозиториев для удобства работы с сервером"
+	echo -e " 2. Создание unit для Shredder Desktop, автоматический запуск. (Посмотреть статус: # dsus)"
+	echo -e " 3. Установка и управление WireGuard"
+	echo -e " 4. Установка и управление OpenVPN"
+	echo -e " 5. Установка и управление Transmission (# transmission / удаление # tr_rm)"
+	echo -e " -- Забрать загруженные файлы с торрентов # start_light_server"
+	echo -e " -- Остановить HTTP доступ к папке Downloads # stop_light_server"
+	echo -e " 6. Проверка обновлений или установка GitHub"
+	echo -e " 7. Установка TOR и Privoxy"
+	echo -e " 8. Опция 3"
+	echo -e " 9. Опция 3"
+	echo -e " 0. Выход"
+	echo -e 
 }
 
 # Функция для обработки выбранной опции
@@ -39,41 +39,42 @@ handle_option() {
 	case $choice in
 		1)
 			echo -e " Вы выбрали Опцию 1"
-			echo " 1. Пакет программ и репозиториев для удобства работы с сервером"\n
+			echo -e " 1. Пакет программ и репозиториев для удобства работы с сервером"\n
 			# Здесь можете добавить код для выполнения действий для Опции 1
 			/root/vdsetup.2/bin/utility/install/epel_repo_pack.sh ;
 			;;
 		2)
 			echo -e " Вы выбрали Опцию 2"
-			echo " 2. Создание unit для Shredder Desktop, автоматический запуск. (Посмотреть статус: # dsus)"\n
+			echo -e " 2. Создание unit для Shredder Desktop, автоматический запуск. (Посмотреть статус: # dsus)"\n
 			
 			/root/vdsetup.2/bin/utility/install/shredder/shredder_unit.sh ;
 			;;
 		3)
 			echo -e " Вы выбрали Опцию 3"
-			echo " 3. Установка и управление WireGuard"
+			echo -e " 3. Установка и управление WireGuard"
 			/root/vdsetup.2/bin/utility/install/vpn/wireguard-install.sh ;
 			;;
 		4)
 			echo -e " Вы выбрали Опцию 4"
-			echo " 4. Установка и управление OpenVPN"
+			echo -e " 4. Установка и управление OpenVPN"
 			/root/vdsetup.2/bin/utility/install/vpn/openvpn-install.sh ;
 			;;
 		5)
 			echo -e " Вы выбрали Опцию 5"
-			echo " 5. Установка и управление Transmission (# transmission / удаление # tr_rm)"
-			echo " -- Забрать загруженные файлы с торрентов # start_light_server"
-			echo " -- Остановить HTTP доступ к папке Downloads # stop_light_server"
+			echo -e " 5. Установка и управление Transmission (# transmission / удаление # tr_rm)"
+			echo -e " -- Забрать загруженные файлы с торрентов # start_light_server"
+			echo -e " -- Остановить HTTP доступ к папке Downloads # stop_light_server"
 			transmission ;
 			;;
 		6)
 			echo -e " Вы выбрали Опцию 6"
-			echo " 6. Проверка обновлений или установка GitHub"
+			echo -e " 6. Проверка обновлений или установка GitHub"
 			/root/vdsetup.2/bin/utility/install/github.sh ;
 			;;
 		7)
 			echo -e " Вы выбрали Опцию 7"
-			# Здесь можете добавить код для выполнения действий для Опции 3
+			echo -e " 7. Установка TOR и Privoxy"
+			/root/vdsetup.2/bin/utility/install/tor/tor_installer.sh ;
 			;;
 		8)
 			echo -e " Вы выбрали Опцию 8"
@@ -85,11 +86,11 @@ handle_option() {
 			;;
 
 		0)
-			echo " До свидания!"
+			echo -e " До свидания!"
 			exit 0 ;
 			;;
 		*)
-			echo " Недопустимая опция"
+			echo -e " Недопустимая опция"
 			;;
 	esac
 }
