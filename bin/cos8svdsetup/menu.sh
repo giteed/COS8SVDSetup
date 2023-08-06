f272e24928b08ab8797dd2599d8db6679b70ff01
SERGE <serge@macbook-pro-serge-2.local>
2023-08-06 19:01:33 +0300

    .

--- bin/cos8svdsetup/menu.sh
+++ bin/cos8svdsetup/menu.sh
@@ -7,12 +7,11 @@
 # --> Эта ссылка на функцию проверяет, запущен-ли скрипт с правами суперпользователя (root) в Linux.
 . /root/vdsetup.2/bin/functions/run_as_root.sh ;
 
-
 check_os_compatibility() {
 	expected_os="CentOS Stream"
 	expected_version="8"
 
-	current_os=$(cat /etc/centos-release | awk '{print $1, $4}')
+	current_os=$(cat /etc/centos-release | awk '{print $1}')
 	current_version=$(cat /etc/centos-release | awk '{print $4}')
 
 	echo "Ожидаемая ОС: $expected_os"
@@ -20,7 +19,7 @@ check_os_compatibility() {
 	echo "Текущая ОС: $current_os"
 	echo "Текущая версия: $current_version"
 
-	if [[ "$current_os" == "$expected_os" ]] && [[ "$current_version" == "$expected_version" ]]; then
+	if [[ "$current_os" == "$expected_os" || "$current_os" == "CentOS" ]] && [[ "$current_version" == "$expected_version" ]]; then
 		echo "Данный скрипт был написан и тестировался с CentOS Stream release 8"
 		echo "Версия вашей ОС: $current_os $current_version"
 	else
