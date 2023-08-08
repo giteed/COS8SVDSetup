29f4047bd7bcc328fcab067e59a2508dd8fcc1b2
SERGE <serge@macbook-pro-serge-2.local>
2023-08-09 01:58:07 +0300

    .

--- bin/cos8svdsetup/menu.sh
+++ bin/cos8svdsetup/menu.sh
@@ -320,7 +320,7 @@ handle_option() {
 		13 | full_tcp_port_scan)
 			echo -e " Вы выбрали Опцию 13"
 			echo -e " 13. Простое сканирование всех localhost TCP/UDP 65535 портов\n с выводом во временный файл. # full_tcp_port_scan и # fw_i_r"
-			full_tcp_port_scan && who ;
+			full_tcp_port_scan ;
 			echo -e 
 			;;
 		13a | nmapp)
