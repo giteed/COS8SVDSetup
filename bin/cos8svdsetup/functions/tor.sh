e727297712e3449501ac47d38e997045ffbe793d
SERGE <serge@macbook-pro-serge-2.local>
2023-08-09 01:24:32 +0300

    .

--- bin/cos8svdsetup/functions/tor.sh
+++ bin/cos8svdsetup/functions/tor.sh
@@ -273,4 +273,8 @@ function wgetrc_config_revert() {
   
     echo -e "${cr}\n\n Полное сканирование завершено.\n Открытые порты сохранены в файл: $output_file\n"
     ttb=$(echo "$(cat $output_file)") && lang="nix" && bpn_lang
+    
+    ttb=$(echo "$(nmap localhost -sT -sU -p-)") && lang="nix" && bpn_lang
+    
+    
   }
