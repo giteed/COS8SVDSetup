11ecbc3439dc11b435df492b5784e90bcc300d6a
SERGE <serge@macbook-pro-serge-2.local>
2023-08-09 12:35:47 +0300

    .

--- bin/cos8svdsetup/functions/tor.sh
+++ bin/cos8svdsetup/functions/tor.sh
@@ -275,6 +275,7 @@ function wgetrc_config_revert() {
     echo -e "${cr}\n\n Полное сканирование завершено.\n Открытые порты сохранены в файл: $output_file\n"
     ttb=$(echo "$(cat $output_file)") && lang="nix" && bpn_lang ;
     ttb=$(echo "$(nmap localhost -sT -sU -p-)") && lang="nix" && bpn_lang ;
+    wport . ;
     fw_i_r ;
     
     echo -e "\n/usr/bin/who -H\n" &&  who -H | bat  --paging=never -l c -p && wport ssh && ( ttb=$( echo -e "\n ⎧ Убить Процесс по PID: # killl \"PID\" \n ⎩ Справка: # killl -h" ) && lang="nix" && bpn_p_lang) && etc_passwd ;
