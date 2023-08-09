efc047b48a02eced26f4e2fcb9f4a449e2432f6d
SERGE <serge@macbook-pro-serge-2.local>
2023-08-09 12:19:55 +0300

    .

--- bin/cos8svdsetup/.help/.load_help_func.sh
+++ bin/cos8svdsetup/.help/.load_help_func.sh
@@ -6,4 +6,5 @@
 	. /root/vdsetup.2/bin/.help/core_grubby_help.sh
 # --> Установка и удаление репозиториев/пакетов (packages) на CentOS.
 	. /root/vdsetup.2/bin/.help/rpm_readme.sh
-	
+# --> Про функцию, предназначенную для определения доступных портов Tor на локальной машине и сохранения этой информации в переменной tor_port
+	. /root/vdsetup.2/bin/.help/tor_port_ch_help
