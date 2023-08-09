19c2166c6169e6496b63a2c3915f91070b7931f5
SERGE <serge@macbook-pro-serge-2.local>
2023-08-10 00:25:35 +0300

    .

--- bin/cos8svdsetup/menu.sh
+++ bin/cos8svdsetup/menu.sh
@@ -91,7 +91,7 @@ print_menu() {
 	echo -e " -- 7e Проверяет доступность \".onion\" адреса через \"TOR\" # tor_onion_test"
 	echo -e " -- 7f Перезапустит \"TOR\" и проверит его работу # tor_restart_status"
 	echo -e " -- 7g Проверит работу \"TOR\" и \"ip\" соединения или перезапустит если нет ответа от \"socks5\" # tor_check_ip_or_restart"
-	echo -e " -- 7h Проверит работу \"wget\" через \"socks5\" # tor_check_ip_wget"
+	echo -e " -- 7h Проверит работу \"wget\" через \"TOR\" \"socks5\" # tor_check_ip_wget"
 	echo -e " -- 7i Создает отдельный интерфейс с именем \"tor0\" # tor_Interface_unit_reinstall [intrface_name] (в разработке)"
 	echo -e " -- 7j Показывает статус \"tor\" интерфейса # status_tor_service"
 	echo -e " -- 7k Жестко перезапустит несколько сервисов включая \"TOR\" # tor-restart"
@@ -276,7 +276,7 @@ handle_option() {
 			;;
 		7h | tor_check_ip_wget)
 			echo -e " Вы выбрали Опцию 7h"
-			echo -e " -- 7h Проверит работу \"wget\" через \"socks5\" # tor_check_ip_wget"
+			echo -e " -- 7h Проверит работу \"wget\" через \"TOR\" \"socks5\" # tor_check_ip_wget"
 			echo -e 
 			tor_check_ip_wget ;
 			;;
