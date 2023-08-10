3ae8898d2a9e61d443936512ab0e8dd55855cabf
SERGE <serge@macbook-pro-serge-2.local>
2023-08-10 14:45:06 +0300

    .

--- bin/cos8svdsetup/utility/install/downloader_tor_fex/scriptsd/download.sh
+++ bin/cos8svdsetup/utility/install/downloader_tor_fex/scriptsd/download.sh
@@ -109,7 +109,7 @@ function make_wget_url_for_html_with_cdn_url() {
 			
 			mkdir -p "${WORK_DIR}"/downloaded_cdn/ && mv "$output_cdn_download_file" "${WORK_DIR}"/downloaded_cdn/one_url_with_cdn_download.sh && chmod +x "${WORK_DIR}"/downloaded_cdn/one_url_with_cdn_download.sh ;
 			rm -rf "${WORK_DIR}"/temp_download_dir/ ;
-			done_cleared ;
+			
 	}
 
 
@@ -136,6 +136,7 @@ function start() {
 		then
 			# Если файл пуст, удаляем его и завершаем скрипт
 			rm "${WORK_DIR}"/temp_list_for_download_from_"$domain"_all_urls.txt
+			done_cleared ;
 			exit
 		else
 			# Если файл не пуст, вызываем функции:
